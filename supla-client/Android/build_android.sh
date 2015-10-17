aec94431a84db1b246092f2d1bace8a957bd4d72
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2015-10-17 19:51:05 +0200

    android support

--- supla-client/Android/build_android.sh
+++ supla-client/Android/build_android.sh
@@ -8,34 +8,8 @@
 # ============================================================================
 
 NDK_ROOT="/Users/AC/Devel/projekty/Android/ndk-r10c"
-BUILD_CFG="../Release"
-IOS_OPENSSL_LIB_INC="../../../openssl/include"
 
-BUILD_DIR="./"$BUILD_CFG
-DEVELOPER=`xcode-select -print-path`
-
-if [ ! -d $BUILD_DIR ]; then
-    echo "Build dir does not exists! " $BUILD_DIR;
-    exit;
-fi
-
-PATH="$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/:$PATH"
-export SYS_ROOT="$NDK_ROOT/platforms/android-12/arch-arm/"
-export GCC="arm-linux-androideabi-gcc -march=armv7-a -I${IOS_OPENSSL_LIB_INC} --sysroot=$SYS_ROOT"
-export CC=$GCC
-export LD="arm-linux-androideabi-ld"
-export AR="arm-linux-androideabi-ar"
-export RANLIB="arm-linux-androideabi-ranlib"
-export STRIP="arm-linux-androideabi-strip"
 export OPENSSL_ANDROID=/Users/AC/Public/openssl
-
-P=`pwd`
-
-cd $BUILD_DIR
-make clean
-make
-
-cd $P
 
 $NDK_ROOT/ndk-build clean
 $NDK_ROOT/ndk-build -C ./ NDK_DEBUG=1
