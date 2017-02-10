4bfdf35816f280e827a1dc52a89fcf9920bbeaca
Przemek Zygmunt <przemek@supla.org>
2017-02-10 19:07:10 +0100

    db

--- supla-server/src/db.h
+++ supla-server/src/db.h
@@ -81,6 +81,7 @@ public:
 	void add_temperature(int ChannelID, double temperature);
 	void add_temperature_and_humidity(int ChannelID, double temperature, double humidity);
 
+
 	bool get_oauth_user(char *access_token, int *OAuthUserID, int *UserID, int *expires_at);
 
 };
