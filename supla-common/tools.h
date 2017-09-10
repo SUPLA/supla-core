2e2ad573c1ccfae82a7ffe61186c2ca1c497befb
Przemek Zygmunt <przemek@supla.org>
2017-09-10 17:30:11 +0200

    License update

--- supla-common/tools.h
+++ supla-common/tools.h
@@ -1,4 +1,6 @@
 /*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
@@ -13,7 +15,6 @@
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 
 #ifndef suplatools_H_
 #define suplatools_H_
--- supla-dev/src/devconnection.h
+++ supla-dev/src/devconnection.h
@@ -1,4 +1,6 @@
 /*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
