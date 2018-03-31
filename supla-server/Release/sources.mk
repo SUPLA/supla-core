7ee8d771d97a0c6f6fbf8b075504c51d7d3fc3b9
Przemek Zygmunt <przemek@supla.org>
2018-03-31 19:03:21 +0200

    New source files (clientchannelgroup)

--- supla-server/Release/sources.mk
+++ supla-server/Release/sources.mk
@@ -24,5 +24,6 @@ CPP_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src \
+src/clientchannelgroup \
 src/crypt_blowfish \
 
--- supla-server/Test/sources.mk
+++ supla-server/Test/sources.mk
@@ -24,6 +24,7 @@ CPP_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src \
+src/clientchannelgroup \
 src/crypt_blowfish \
 src/test \
 src/test/gtest \
