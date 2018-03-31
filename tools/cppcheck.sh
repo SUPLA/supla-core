8121b4ea87f28f1852fe6916ccc7e3b9b043c581
Przemek Zygmunt <przemek@supla.org>
2018-03-31 17:17:30 +0200

    Show version

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cppcheck --version
 cppcheck --inline-suppr --error-exitcode=1 -q -i ./supla-server/src/test ./supla-server/src/*  && \
 cppcheck --inline-suppr --error-exitcode=1 -q -i ./supla-scheduler/src/test ./supla-scheduler/src/* && \
 exit 0
