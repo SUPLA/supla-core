ae64414490574f6417dc5e958d6bfd8ca7b03cfd
Przemek Zygmunt <przemek@supla.org>
2018-04-05 18:01:19 +0200

    afl v1.0

--- supla-afl/Debug/src/subdir.mk
+++ supla-afl/Debug/src/subdir.mk
@@ -3,17 +3,43 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/eh.c \
+../src/lck.c \
+../src/log.c \
+../src/proto.c \
+../src/srpc.c 
+
 CPP_SRCS += \
 ../src/supla-afl.cpp 
 
 OBJS += \
+./src/eh.o \
+./src/lck.o \
+./src/log.o \
+./src/proto.o \
+./src/srpc.o \
 ./src/supla-afl.o 
+
+C_DEPS += \
+./src/eh.d \
+./src/lck.d \
+./src/log.d \
+./src/proto.d \
+./src/srpc.d 
 
 CPP_DEPS += \
 ./src/supla-afl.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	/usr/src/afl/afl-gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
--- supla-afl/src/supla-afl.cpp
+++ supla-afl/src/supla-afl.cpp
@@ -16,4 +16,79 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-int main() { return 0; }
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "eh.h"
+#include "log.h"
+#include "srpc.h"
+
+char debug_mode = 1;
+char run_as_daemon = 0;
+int fd = -1;
+int n = 10;
+
+int supla_read(void *buf, int count, void *sc) {
+  count = read(fd, buf, count);
+  // supla_log(LOG_DEBUG, "read=%i", count);
+  if (count == 0) {
+    count = -1;
+  }
+
+  return count;
+}
+
+int supla_write(void *buf, int count, void *sc) { return count; }
+
+void supla_on_remote_call_received(void *_srpc, unsigned int rr_id,
+                                   unsigned int call_type, void *sc,
+                                   unsigned char proto_version) {
+  supla_log(LOG_DEBUG, "Call: %i, proto: %i, rr_id: %i", call_type,
+            proto_version, rr_id);
+
+  TsrpcReceivedData rd;
+  srpc_getdata(_srpc, &rd, rr_id);
+  n = 10;
+}
+
+void supla_on_version_error(void *_srpc, unsigned char remote_version,
+                            void *sc) {}
+
+int main() {
+  void *srpc = NULL;
+  TEventHandler *eh = eh_init();
+
+  fd = open("/tmp/supla_afl.raw", O_RDONLY);
+
+  if (fd == -1) {
+    supla_log(LOG_DEBUG, "Can't open file /tmp/supla_afl.raw");
+    return 1;
+  }
+
+  TsrpcParams srpc_params;
+  srpc_params_init(&srpc_params);
+  srpc_params.user_params = NULL;
+  srpc_params.data_read = &supla_read;
+  srpc_params.data_write = &supla_write;
+  srpc_params.on_remote_call_received = &supla_on_remote_call_received;
+  srpc_params.on_version_error = &supla_on_version_error;
+  srpc_params.eh = eh;
+  srpc = srpc_init(&srpc_params);
+
+  do {
+    if (srpc_iterate(srpc) == SUPLA_RESULT_FALSE) {
+      // supla_log(LOG_DEBUG, "srpc_iterate(srpc) == SUPLA_RESULT_FALSE");
+      break;
+    }
+    n--;
+  } while (srpc_input_dataexists(srpc) == SUPLA_RESULT_TRUE && n > 0);
+
+  srpc_free(srpc);
+  eh_free(eh);
+
+  close(fd);
+
+  return 0;
+}
