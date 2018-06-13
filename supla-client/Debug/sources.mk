90c61a1e3bb3bf1a94979ae152ae7edd83031990
Przemek Zygmunt <przemek@supla.org>
2018-06-13 16:09:11 +0200

    Makefile update

--- supla-client/Debug/sources.mk
+++ supla-client/Debug/sources.mk
@@ -2,14 +2,14 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-O_SRCS := 
-C_SRCS := 
-S_UPPER_SRCS := 
 OBJ_SRCS := 
 ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
 OBJS := 
-C_DEPS := 
 ARCHIVES := 
+C_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
--- supla-client/Debug/src/subdir.mk
+++ supla-client/Debug/src/subdir.mk
@@ -50,7 +50,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	gcc $(PARAMS)  -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc $(PARAMS)  -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
