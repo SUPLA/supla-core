1cd1511a93b55b2f27b61864db73e27a19494426
Przemek Zygmunt <przemek@supla.org>
2018-10-16 13:48:25 +0200

    db version increment

--- supla-server/src/supla-server.cpp
+++ supla-server/src/supla-server.cpp
@@ -79,7 +79,7 @@ int main(int argc, char *argv[]) {
 
   {
     database *db = new database();
-    if (!db->check_db_version("20180814155501")) {
+    if (!db->check_db_version("20181007112610")) {
       delete db;
       database::mainthread_end();
       goto exit_fail;
