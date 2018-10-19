19aedcf7ac94e9717d9fbe8456ae4094ebadf610
Przemek Zygmunt <przemek@supla.org>
2018-10-19 13:54:08 +0200

    Return zero if less than 1

--- supla-common/srpc.c
+++ supla-common/srpc.c
@@ -1642,7 +1642,8 @@ _supla_int_t SRPC_ICACHE_FLASH srpc_cs_async_oauth_token_request(void *_srpc) {
 
 _supla_int_t SRPC_ICACHE_FLASH srpc_cs_async_oauth_token_request_result(
     void *_srpc, TSC_OAuthTokenRequestResult *result) {
-  if (result == NULL || result->Token.TokenSize > SUPLA_OAUTH_TOKEN_MAXSIZE) {
+  if (result == NULL || result->Token.TokenSize < 1 ||
+      result->Token.TokenSize > SUPLA_OAUTH_TOKEN_MAXSIZE) {
     return 0;
   }
 
