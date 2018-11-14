5e416a354241ec6eb982d9eccc7c7fcf62486d2f
Przemek Zygmunt <przemek@supla.org>
2018-11-14 16:33:08 +0100

    Missing files

---
+++ supla-server/Debug/src/http/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/http/trivial_http.cpp \
+../src/http/trivial_https.cpp 
+
+OBJS += \
+./src/http/trivial_http.o \
+./src/http/trivial_https.o 
+
+CPP_DEPS += \
+./src/http/trivial_http.d \
+./src/http/trivial_https.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/http/%.o: ../src/http/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	$(CXX) -D__DEBUG=1 -D__OPENSSL_TOOLS=1 -D__SSOCKET_WRITE_TO_FILE=$(SSOCKET_WRITE_TO_FILE) -D__BCRYPT=1 -I/usr/include/mysql -I../src/device -I../src/user -I../src -I$(SSLDIR)/include -I../src/client -O2 -g3 -Wall -c -fmessage-length=0 -fstack-protector-all -D_FORTIFY_SOURCE=2 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
