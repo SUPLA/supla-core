78464cc9d98b774172cf91f7f3f0bca70f0e2067
Przemek Zygmunt <przemek@supla.org>
2018-11-14 16:10:37 +0100

    http directory added to the lint check

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,6 +3,7 @@
 cpplint ./supla-server/src/* && \
 cpplint ./supla-server/src/device/* && \
 cpplint ./supla-server/src/client/* && \
+cpplint ./supla-server/src/http/* && \
 cpplint ./supla-server/src/test/* && \
 cpplint ./supla-scheduler/src/* && \
 cpplint ./supla-scheduler/src/test/* && \
