576c33f4f36c20bcd8cfd2829749a6f1f799f35c
Przemek Zygmunt <przemek@supla.org>
2018-11-14 16:03:45 +0100

    Increase eh_wait to 30sec.

--- supla-server/src/serverconnection.cpp
+++ supla-server/src/serverconnection.cpp
@@ -617,7 +617,7 @@ void serverconnection::execute(void *sthread) {
             ssocket_is_secure(ssd));
 
   while (sthread_isterminated(sthread) == 0) {
-    eh_wait(eh, 10000000);
+    eh_wait(eh, 30000000);
 
     if (srpc_iterate(_srpc) == SUPLA_RESULT_FALSE) {
       // supla_log(LOG_DEBUG, "srpc_iterate(_srpc) == SUPLA_RESULT_FALSE");
