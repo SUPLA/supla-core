617884fd68cfd3cc1cf004797ec42dbf43c80aef
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2018-11-28 11:10:02 +0100

    db version increment

--- supla-server/src/supla-server.cpp
+++ supla-server/src/supla-server.cpp
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]) {
 
   {
     database *db = new database();
-    if (!db->check_db_version("20181119121610")) {
+    if (!db->check_db_version("20181126225634")) {
       delete db;
       database::mainthread_end();
       goto exit_fail;
