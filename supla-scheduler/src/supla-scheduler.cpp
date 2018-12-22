3d307c3f700a988c8bf4f536f1a07eeb6745a276
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2018-12-22 15:47:03 +0100

    Db version increment

--- supla-scheduler/src/supla-scheduler.cpp
+++ supla-scheduler/src/supla-scheduler.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
 
   {
     database *db = new database();
-    if (!db->check_db_version("20181205092324")) {
+    if (!db->check_db_version("20181222001450")) {
       delete db;
       database::mainthread_end();
       goto exit_fail;
