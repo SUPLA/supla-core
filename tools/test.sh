13342bffefa9226f9b82383b6fc3ba689417023f
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-01 17:36:30 +0100

    sudo

--- tools/test.sh
+++ tools/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-[ -d /etc/supla-server ] || mkdir /etc/supla-server
-[ -e /etc/supla-server/supla-test.cfg ] || touch /etc/supla-server/supla-test.cfg
+[ -d /etc/supla-server ] || sudo mkdir /etc/supla-server
+[ -e /etc/supla-server/supla-test.cfg ] || sudo touch /etc/supla-server/supla-test.cfg
 
 cd supla-server/Test && \
 make clean && make all && \
