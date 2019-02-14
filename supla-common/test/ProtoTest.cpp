9017caaa77adf93bad9f7ace28f5bd9d64c14fe0
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-14 11:57:34 +0100

    UTests for new structures

--- supla-common/test/ProtoTest.cpp
+++ supla-common/test/ProtoTest.cpp
@@ -108,6 +108,16 @@ TEST_F(ProtoTest, check_size_of_structures_and_types) {
   ASSERT_LE(sizeof(TElectricityMeter_Value), SUPLA_CHANNELVALUE_SIZE);
   ASSERT_LE(sizeof(TElectricityMeter_ExtendedValue),
             SUPLA_CHANNELEXTENDEDVALUE_SIZE);
+
+  ASSERT_EQ(4, sizeof(TThermostat_Time));
+  ASSERT_EQ(114, sizeof(TThermostat_ExtendedValue));
+  ASSERT_EQ(6, sizeof(TThermostat_Value));
+  ASSERT_EQ(105, sizeof(TThermostat_Configuration));
+
+  ASSERT_LE(sizeof(TThermostat_ExtendedValue), SUPLA_CHANNELEXTENDEDVALUE_SIZE);
+  ASSERT_LE(sizeof(TThermostat_Value), SUPLA_CHANNELVALUE_SIZE);
+  ASSERT_LE(sizeof(TThermostat_Configuration), SUPLA_CALCFG_DATA_MAXSIZE);
+
 }
 
 TEST_F(ProtoTest, init) {
