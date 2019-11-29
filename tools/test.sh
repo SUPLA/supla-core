acedb9fd7010f60674f43274fc05bf59d120f6f0
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-11-29 17:24:11 +0100

    Test script improvements

--- tools/test.sh
+++ tools/test.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
+set -e
+
 [ -d /etc/supla-server ] || sudo mkdir /etc/supla-server
 [ -e /etc/supla-server/supla-test.cfg ] || sudo touch /etc/supla-server/supla-test.cfg
 
-cd supla-server/Test && \
-make clean && make all && \
-./supla-server && \
-(../../tools/valgrind-noundef.sh ./supla-server 2>&1 | grep "All heap blocks were freed -- no leaks are possible" ) && \
-cd ../Release && \
-make clean && make all && \
-cd ../../supla-scheduler/Test && \
-make clean && make all && \
-./supla-scheduler && \
-cd ../Release && \
-make clean && make all && \
-cd ../../supla-dev/Release && \
-make clean && make && \
-cd ../../supla-console-client/Debug && \
-make clean && make && \
-echo OK && \
+cd supla-server/Test 
+make clean && make all 
+./supla-server 
+
+(../../tools/valgrind-noundef.sh ./supla-server 2>&1 \
+   | grep "All heap blocks were freed -- no leaks are possible" ) \
+|| (echo "Memory leak error!" && exit 1)
+
+cd ../Release 
+make clean && make all 
+cd ../../supla-scheduler/Test 
+make clean && make all 
+./supla-scheduler 
+cd ../Release 
+make clean && make all 
+cd ../../supla-dev/Release 
+make clean && make 
+cd ../../supla-console-client/Debug 
+make clean && make 
+
+echo OK 
 exit 0
-
-echo FAILED
-exit 1
-
-
