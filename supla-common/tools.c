4afa31f0d5d23d9051ebf908368ba34d8a43cf13
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-12-31 18:30:02 +0100

    Memory leak caught on ubuntu bionic version

--- supla-common/tools.c
+++ supla-common/tools.c
@@ -588,7 +588,6 @@ char *st_openssl_base64_encode(char *src, int src_len) {
   bio = BIO_new(BIO_s_mem());
   bio = BIO_push(b64, bio);
   BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-  (void)BIO_set_close(bio, BIO_NOCLOSE);
 
   BIO_write(bio, src, src_len);
   (void)BIO_flush(bio);
@@ -600,7 +599,6 @@ char *st_openssl_base64_encode(char *src, int src_len) {
   result[bufferPtr->length] = 0;
 
   BIO_free_all(bio);
-  BUF_MEM_free(bufferPtr);
 
   return result;
 }
