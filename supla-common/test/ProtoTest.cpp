358e553f32f077fa01298f24f9bb3e6988fda75d
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-04-05 15:30:36 +0200

    sizeof(TCalCfg_ZWave_Node) == 54

--- supla-common/test/ProtoTest.cpp
+++ supla-common/test/ProtoTest.cpp
@@ -143,7 +143,7 @@ TEST_F(ProtoTest, check_size_of_structures_and_types) {
   ASSERT_EQ((unsigned int)4, sizeof(TCS_DeviceReconnectRequest));
   ASSERT_EQ((unsigned int)5, sizeof(TSC_DeviceReconnectRequestResult));
 
-  ASSERT_EQ((unsigned int)53, sizeof(TCalCfg_ZWave_Node));
+  ASSERT_EQ((unsigned int)54, sizeof(TCalCfg_ZWave_Node));
   ASSERT_LE(sizeof(TCalCfg_ZWave_Node),
             (unsigned int)SUPLA_CALCFG_DATA_MAXSIZE);
   ASSERT_LE(sizeof(unsigned char), (unsigned int)SUPLA_CALCFG_DATA_MAXSIZE);
