a36137f71a01c753704cbfdc0fc181ea817a7aa5
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-04-13 15:04:24 +0200

    set -e

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,22 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-cpplint ./supla-server/src/* && \
-cpplint ./supla-server/src/amazon/* && \
-cpplint ./supla-server/src/client/* && \
-cpplint ./supla-server/src/device/* && \
-cpplint ./supla-server/src/google/* && \
-cpplint ./supla-server/src/http/* && \
-cpplint ./supla-server/src/user/* && \
-cpplint ./supla-server/src/test/* && \
-cpplint ./supla-scheduler/src/* && \
-cpplint ./supla-scheduler/src/test/* && \
-cpplint ./supla-dev/src/* && \
-cpplint ./supla-client/src/* && \
-cpplint ./supla-console-client/src/* && \
-cpplint ./supla-console-client/src/supla-client-lib/* && \
-cpplint ./supla-afl/src/* && \
-echo OK && \
-exit 0
+set -e
 
-echo FAILED
-exit 1
+cpplint ./supla-server/src/* 
+cpplint ./supla-server/src/amazon/* 
+cpplint ./supla-server/src/client/* 
+cpplint ./supla-server/src/device/* 
+cpplint ./supla-server/src/google/* 
+cpplint ./supla-server/src/http/* 
+cpplint ./supla-server/src/user/* 
+cpplint ./supla-server/src/test/* 
+cpplint ./supla-scheduler/src/* 
+cpplint ./supla-scheduler/src/test/* 
+cpplint ./supla-dev/src/* 
+cpplint ./supla-client/src/* 
+cpplint ./supla-console-client/src/* 
+cpplint ./supla-console-client/src/supla-client-lib/* 
+cpplint ./supla-afl/src/* 
+echo OK
