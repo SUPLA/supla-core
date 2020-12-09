17818e3088d43e280ecbd227d30a59b09b4132a4
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-12-09 12:28:21 +0100

    CodeStyle [build/include_what_you_use]

--- supla-server/src/test/mqtt/MqttTopicCmpTest.cpp
+++ supla-server/src/test/mqtt/MqttTopicCmpTest.cpp
@@ -16,6 +16,9 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <list>
+#include <string>
+
 #include "MqttTopicCmpTest.h"
 #include "mqtt_channelandstate_message_provider.h"
 
