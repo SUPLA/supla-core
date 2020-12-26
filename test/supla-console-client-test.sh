e9e8235bb0bb3af9b8373b2011173ebd5f7e6739
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-12-26 13:12:37 +0100

    cd Release

--- test/supla-console-client-test.sh
+++ test/supla-console-client-test.sh
@@ -39,6 +39,7 @@ sleep 2
 
 if ! ps ax|grep supla-server|grep -v grep > /dev/null; then
 grep supla-server /var/log/syslog
+cd ./supla-server/Release
 ../../tools/valgrind-noundef.sh "./supla-server -c /etc/supla-server/supla-test.cfg"
 echo "supla-server did not start!"
 exit 1
