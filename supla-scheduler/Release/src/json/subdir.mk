7a6d894501b09733b9e29502bd3115883b3a8bcc
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-01-28 22:26:33 +0100

    Missing mk files

---
+++ supla-scheduler/Release/src/json/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/json/cJSON.c \
+../src/json/cJSON_Utils.c 
+
+OBJS += \
+./src/json/cJSON.o \
+./src/json/cJSON_Utils.o 
+
+C_DEPS += \
+./src/json/cJSON.d \
+./src/json/cJSON_Utils.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/json/%.o: ../src/json/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -I$(INCMYSQL) -O3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
