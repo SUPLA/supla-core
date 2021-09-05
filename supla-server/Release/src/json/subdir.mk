e96e9a266a0e7e2f942bca9b1421090db416fe6a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-09-06 00:55:22 +0200

    channel_json_config empty class added

--- supla-server/Release/src/json/subdir.mk
+++ supla-server/Release/src/json/subdir.mk
@@ -7,13 +7,20 @@ C_SRCS += \
 ../src/json/cJSON.c \
 ../src/json/cJSON_Utils.c 
 
+CPP_SRCS += \
+../src/json/channeljsonconfig.cpp 
+
 OBJS += \
 ./src/json/cJSON.o \
-./src/json/cJSON_Utils.o 
+./src/json/cJSON_Utils.o \
+./src/json/channeljsonconfig.o 
 
 C_DEPS += \
 ./src/json/cJSON.d \
 ./src/json/cJSON_Utils.d 
+
+CPP_DEPS += \
+./src/json/channeljsonconfig.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -21,6 +28,13 @@ src/json/%.o: ../src/json/%.c src/json/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
 	gcc -D__BCRYPT=1 -DSPROTO_WITHOUT_OUT_BUFFER -DSRPC_WITHOUT_OUT_QUEUE -DUSE_DEPRECATED_EMEV_V1 -D__OPENSSL_TOOLS=1 -I$(SSLDIR)/include -I../src/mqtt -I../src -I../src/user -I../src/device -I../src/client -O3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/json/%.o: ../src/json/%.cpp src/json/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -D__BCRYPT=1 -DSPROTO_WITHOUT_OUT_BUFFER -DSRPC_WITHOUT_OUT_QUEUE -DUSE_DEPRECATED_EMEV_V1 -D__OPENSSL_TOOLS=1 -I$(INCMYSQL) -I../src/mqtt -I../src/client -I../src/user -I../src/device -I../src -I$(SSLDIR)/include -O3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
