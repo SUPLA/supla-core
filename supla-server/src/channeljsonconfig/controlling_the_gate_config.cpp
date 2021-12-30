08f3e4dcbdf3bd14c1c1da866369e45678e11913
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-12-30 23:19:15 +0100

    controlling_the_gate_config class implementation

---
+++ supla-server/src/channeljsonconfig/controlling_the_gate_config.cpp
@@ -0,0 +1,50 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "controlling_the_gate_config.h"
+
+#define NOA_MIN 1
+#define NOA_MAX 5
+#define NOA_DEFAULT 5
+
+// static
+const char controlling_the_gate_config::number_of_attempts_key[] =
+    "numberOfAttemptsToOpenOrClose";
+
+controlling_the_gate_config::controlling_the_gate_config(void)
+    : channel_json_config() {}
+
+controlling_the_gate_config::controlling_the_gate_config(
+    channel_json_config *root)
+    : channel_json_config(root) {}
+
+int controlling_the_gate_config::get_number_of_attempts(void) {
+  cJSON *root = get_user_root();
+  if (!root) {
+    return NOA_DEFAULT;
+  }
+
+  cJSON *value = cJSON_GetObjectItem(root, number_of_attempts_key);
+  if (value && cJSON_IsNumber(value)) {
+    if (value->valueint >= NOA_MIN && value->valueint <= NOA_MAX) {
+      return value->valueint;
+    }
+  }
+
+  return NOA_DEFAULT;
+}
