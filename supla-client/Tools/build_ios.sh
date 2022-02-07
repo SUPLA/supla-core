e2d57f8412ca32b6d9ee9d4b3452d3a4b9f7be34
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-02-07 21:37:05 +0100

    iOS SDK_VERSION 9.0

--- supla-client/Tools/build_ios.sh
+++ supla-client/Tools/build_ios.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
-SDK_VERSION="8.2"
+SDK_VERSION="9.0"
 BUILD_CFG="Release"
 IOS_OPENSSL_LIB_INC="/Users/przemek/CProjects/openssl/OpenSSL-for-iPhone/include"
 
