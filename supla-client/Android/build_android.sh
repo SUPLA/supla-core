736fc9dbc4320964ce2653cbe4db01fe8f511bce
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-02-09 21:02:00 +0100

    NDK r23b.

--- supla-client/Android/build_android.sh
+++ supla-client/Android/build_android.sh
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-[ -z "$NDK_ROOT" ] && NDK_ROOT=$(echo ~/Library/Android/ndk-r14b)
+[ -z "$NDK_ROOT" ] && NDK_ROOT=$(echo ~/Library/Android/ndk-r23b)
 [ -z "$SUPLA_ANDROID" ] && SUPLA_ANDROID=$(echo ~/StudioProjects/supla-android)
 [ -z "$OPENSSL_ANDROID" ] && export OPENSSL_ANDROID=$(echo ~/CProjects/openssl/OpenSSL-for-Android)
 
