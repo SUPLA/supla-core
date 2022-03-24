e4c659a186c9fce5a42e93522cd0cf906755fb46
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-03-24 23:54:26 +0100

    supla_caller class implementation

---
+++ supla-server/src/caller.h
@@ -0,0 +1,58 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef CALLER_H_
+#define CALLER_H_
+
+enum _callerType_e {
+  ctUnknown,
+  ctDevice,
+  ctClient,
+  ctIPC,
+  ctAmazonAlexa,
+  ctGoogleHome,
+  ctActionTrigger,
+  ctScene
+};
+
+class supla_caller {
+ private:
+  supla_caller *parent;
+  _callerType_e type;
+  int id;
+  supla_caller *get_first(void);
+
+ public:
+  explicit supla_caller(const supla_caller &caller);
+  supla_caller(void);
+  explicit supla_caller(_callerType_e type);
+  supla_caller(_callerType_e type, int id);
+  supla_caller(const supla_caller &parent, _callerType_e type);
+  supla_caller(const supla_caller &parent, _callerType_e type, int id);
+  virtual ~supla_caller();
+  int get_id(void);
+  _callerType_e get_type();
+  supla_caller *clone(void) const;
+  int convert_to_sender_id(void);
+  int find(_callerType_e type, int id);
+  int find(_callerType_e type);
+  int stack_size(void);
+  supla_caller &operator=(const supla_caller &caller);
+};
+
+#endif /* CALLER_H_ */
