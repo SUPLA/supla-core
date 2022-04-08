03d9f9fa612ab4c8d00165786a23a8cb40b06367
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-04-09 01:19:19 +0200

    Missing mk file

---
+++ supla-server/Test/src/test/doubles/scene/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/test/doubles/scene/SceneActionExecutorMock.cpp 
+
+CPP_DEPS += \
+./src/test/doubles/scene/SceneActionExecutorMock.d 
+
+OBJS += \
+./src/test/doubles/scene/SceneActionExecutorMock.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/test/doubles/scene/%.o: ../src/test/doubles/scene/%.cpp src/test/doubles/scene/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -D__DEBUG=1 -DUSE_DEPRECATED_EMEV_V1 -D__TEST=1 -D__OPENSSL_TOOLS=1 -D__BCRYPT=1 -I../src -I../src/asynctask -I../src/mqtt -I$(INCMYSQL) -I../src/user -I../src/device -I../src/client -I$(SSLDIR)/include -I../src/test -O2 -g3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -D_FORTIFY_SOURCE=2 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-test-2f-doubles-2f-scene
+
+clean-src-2f-test-2f-doubles-2f-scene:
+	-$(RM) ./src/test/doubles/scene/SceneActionExecutorMock.d ./src/test/doubles/scene/SceneActionExecutorMock.o
+
+.PHONY: clean-src-2f-test-2f-doubles-2f-scene
+
