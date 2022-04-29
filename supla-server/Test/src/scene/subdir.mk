749a7307d360ec97b8d6ca18d702fdf336b5ce31
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-04-30 00:44:01 +0200

    subdir.mk cleanup

--- supla-server/Test/src/scene/subdir.mk
+++ supla-server/Test/src/scene/subdir.mk
@@ -5,34 +5,34 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../src/scene/scene.cpp \
+../src/scene/scene_abstract_repository.cpp \
 ../src/scene/scene_asynctask.cpp \
 ../src/scene/scene_db.cpp \
 ../src/scene/scene_operation.cpp \
 ../src/scene/scene_operations.cpp \
 ../src/scene/scene_repository.cpp \
-../src/scene/scene_repository_factory.cpp \
 ../src/scene/scene_search_condition.cpp \
 ../src/scene/scenes.cpp 
 
 CPP_DEPS += \
 ./src/scene/scene.d \
+./src/scene/scene_abstract_repository.d \
 ./src/scene/scene_asynctask.d \
 ./src/scene/scene_db.d \
 ./src/scene/scene_operation.d \
 ./src/scene/scene_operations.d \
 ./src/scene/scene_repository.d \
-./src/scene/scene_repository_factory.d \
 ./src/scene/scene_search_condition.d \
 ./src/scene/scenes.d 
 
 OBJS += \
 ./src/scene/scene.o \
+./src/scene/scene_abstract_repository.o \
 ./src/scene/scene_asynctask.o \
 ./src/scene/scene_db.o \
 ./src/scene/scene_operation.o \
 ./src/scene/scene_operations.o \
 ./src/scene/scene_repository.o \
-./src/scene/scene_repository_factory.o \
 ./src/scene/scene_search_condition.o \
 ./src/scene/scenes.o 
 
@@ -49,7 +49,7 @@ src/scene/%.o: ../src/scene/%.cpp src/scene/subdir.mk
 clean: clean-src-2f-scene
 
 clean-src-2f-scene:
-	-$(RM) ./src/scene/scene.d ./src/scene/scene.o ./src/scene/scene_asynctask.d ./src/scene/scene_asynctask.o ./src/scene/scene_db.d ./src/scene/scene_db.o ./src/scene/scene_operation.d ./src/scene/scene_operation.o ./src/scene/scene_operations.d ./src/scene/scene_operations.o ./src/scene/scene_repository.d ./src/scene/scene_repository.o ./src/scene/scene_repository_factory.d ./src/scene/scene_repository_factory.o ./src/scene/scene_search_condition.d ./src/scene/scene_search_condition.o ./src/scene/scenes.d ./src/scene/scenes.o
+	-$(RM) ./src/scene/scene.d ./src/scene/scene.o ./src/scene/scene_abstract_repository.d ./src/scene/scene_abstract_repository.o ./src/scene/scene_asynctask.d ./src/scene/scene_asynctask.o ./src/scene/scene_db.d ./src/scene/scene_db.o ./src/scene/scene_operation.d ./src/scene/scene_operation.o ./src/scene/scene_operations.d ./src/scene/scene_operations.o ./src/scene/scene_repository.d ./src/scene/scene_repository.o ./src/scene/scene_search_condition.d ./src/scene/scene_search_condition.o ./src/scene/scenes.d ./src/scene/scenes.o
 
 .PHONY: clean-src-2f-scene
 
