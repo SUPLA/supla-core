459558d55cd9f27f40587956fea58551adb0c720
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-04-30 00:44:01 +0200

    subdir.mk cleanup

--- supla-server/Test/src/distributedobjects/subdir.mk
+++ supla-server/Test/src/distributedobjects/subdir.mk
@@ -9,8 +9,6 @@ CPP_SRCS += \
 ../src/distributedobjects/dobject_observer.cpp \
 ../src/distributedobjects/dobject_registered_observer.cpp \
 ../src/distributedobjects/dobject_related_identifier.cpp \
-../src/distributedobjects/dobject_repository.cpp \
-../src/distributedobjects/dobject_repository_factory.cpp \
 ../src/distributedobjects/dobjects.cpp 
 
 CPP_DEPS += \
@@ -19,8 +17,6 @@ CPP_DEPS += \
 ./src/distributedobjects/dobject_observer.d \
 ./src/distributedobjects/dobject_registered_observer.d \
 ./src/distributedobjects/dobject_related_identifier.d \
-./src/distributedobjects/dobject_repository.d \
-./src/distributedobjects/dobject_repository_factory.d \
 ./src/distributedobjects/dobjects.d 
 
 OBJS += \
@@ -29,8 +25,6 @@ OBJS += \
 ./src/distributedobjects/dobject_observer.o \
 ./src/distributedobjects/dobject_registered_observer.o \
 ./src/distributedobjects/dobject_related_identifier.o \
-./src/distributedobjects/dobject_repository.o \
-./src/distributedobjects/dobject_repository_factory.o \
 ./src/distributedobjects/dobjects.o 
 
 
@@ -46,7 +40,7 @@ src/distributedobjects/%.o: ../src/distributedobjects/%.cpp src/distributedobjec
 clean: clean-src-2f-distributedobjects
 
 clean-src-2f-distributedobjects:
-	-$(RM) ./src/distributedobjects/dobject.d ./src/distributedobjects/dobject.o ./src/distributedobjects/dobject_change_indicator.d ./src/distributedobjects/dobject_change_indicator.o ./src/distributedobjects/dobject_observer.d ./src/distributedobjects/dobject_observer.o ./src/distributedobjects/dobject_registered_observer.d ./src/distributedobjects/dobject_registered_observer.o ./src/distributedobjects/dobject_related_identifier.d ./src/distributedobjects/dobject_related_identifier.o ./src/distributedobjects/dobject_repository.d ./src/distributedobjects/dobject_repository.o ./src/distributedobjects/dobject_repository_factory.d ./src/distributedobjects/dobject_repository_factory.o ./src/distributedobjects/dobjects.d ./src/distributedobjects/dobjects.o
+	-$(RM) ./src/distributedobjects/dobject.d ./src/distributedobjects/dobject.o ./src/distributedobjects/dobject_change_indicator.d ./src/distributedobjects/dobject_change_indicator.o ./src/distributedobjects/dobject_observer.d ./src/distributedobjects/dobject_observer.o ./src/distributedobjects/dobject_registered_observer.d ./src/distributedobjects/dobject_registered_observer.o ./src/distributedobjects/dobject_related_identifier.d ./src/distributedobjects/dobject_related_identifier.o ./src/distributedobjects/dobjects.d ./src/distributedobjects/dobjects.o
 
 .PHONY: clean-src-2f-distributedobjects
 
