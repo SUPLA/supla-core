4338bc8be7110ab8037a2d46c951993b1115c1f9
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-07-02 20:11:03 +0200

    IPC Refactoring. GET-HUMIDITY-VALUE command

--- supla-server/Debug/src/ipc/subdir.mk
+++ supla-server/Debug/src/ipc/subdir.mk
@@ -7,6 +7,7 @@ CPP_SRCS += \
 ../src/ipc/abstract_client_reconnect_command.cpp \
 ../src/ipc/abstract_get_char_command.cpp \
 ../src/ipc/abstract_get_double_command.cpp \
+../src/ipc/abstract_get_humidity_command.cpp \
 ../src/ipc/abstract_get_temperature_command.cpp \
 ../src/ipc/abstract_ipc_command.cpp \
 ../src/ipc/abstract_ipc_ctrl.cpp \
@@ -18,6 +19,7 @@ CPP_SRCS += \
 ../src/ipc/client_reconnect_command.cpp \
 ../src/ipc/get_char_command.cpp \
 ../src/ipc/get_double_command.cpp \
+../src/ipc/get_humidity_command.cpp \
 ../src/ipc/get_temperature_command.cpp \
 ../src/ipc/ipc_ctrl.cpp \
 ../src/ipc/is_channel_connected_command.cpp \
@@ -29,6 +31,7 @@ CPP_DEPS += \
 ./src/ipc/abstract_client_reconnect_command.d \
 ./src/ipc/abstract_get_char_command.d \
 ./src/ipc/abstract_get_double_command.d \
+./src/ipc/abstract_get_humidity_command.d \
 ./src/ipc/abstract_get_temperature_command.d \
 ./src/ipc/abstract_ipc_command.d \
 ./src/ipc/abstract_ipc_ctrl.d \
@@ -40,6 +43,7 @@ CPP_DEPS += \
 ./src/ipc/client_reconnect_command.d \
 ./src/ipc/get_char_command.d \
 ./src/ipc/get_double_command.d \
+./src/ipc/get_humidity_command.d \
 ./src/ipc/get_temperature_command.d \
 ./src/ipc/ipc_ctrl.d \
 ./src/ipc/is_channel_connected_command.d \
@@ -51,6 +55,7 @@ OBJS += \
 ./src/ipc/abstract_client_reconnect_command.o \
 ./src/ipc/abstract_get_char_command.o \
 ./src/ipc/abstract_get_double_command.o \
+./src/ipc/abstract_get_humidity_command.o \
 ./src/ipc/abstract_get_temperature_command.o \
 ./src/ipc/abstract_ipc_command.o \
 ./src/ipc/abstract_ipc_ctrl.o \
@@ -62,6 +67,7 @@ OBJS += \
 ./src/ipc/client_reconnect_command.o \
 ./src/ipc/get_char_command.o \
 ./src/ipc/get_double_command.o \
+./src/ipc/get_humidity_command.o \
 ./src/ipc/get_temperature_command.o \
 ./src/ipc/ipc_ctrl.o \
 ./src/ipc/is_channel_connected_command.o \
@@ -82,7 +88,7 @@ src/ipc/%.o: ../src/ipc/%.cpp src/ipc/subdir.mk
 clean: clean-src-2f-ipc
 
 clean-src-2f-ipc:
-	-$(RM) ./src/ipc/abstract_client_reconnect_command.d ./src/ipc/abstract_client_reconnect_command.o ./src/ipc/abstract_get_char_command.d ./src/ipc/abstract_get_char_command.o ./src/ipc/abstract_get_double_command.d ./src/ipc/abstract_get_double_command.o ./src/ipc/abstract_get_temperature_command.d ./src/ipc/abstract_get_temperature_command.o ./src/ipc/abstract_ipc_command.d ./src/ipc/abstract_ipc_command.o ./src/ipc/abstract_ipc_ctrl.d ./src/ipc/abstract_ipc_ctrl.o ./src/ipc/abstract_ipc_socket_adapter.d ./src/ipc/abstract_ipc_socket_adapter.o ./src/ipc/abstract_is_channel_connected_command.d ./src/ipc/abstract_is_channel_connected_command.o ./src/ipc/abstract_is_client_connected_command.d ./src/ipc/abstract_is_client_connected_command.o ./src/ipc/abstract_is_device_connected_command.d ./src/ipc/abstract_is_device_connected_command.o ./src/ipc/abstract_user_reconnect_command.d ./src/ipc/abstract_user_reconnect_command.o ./src/ipc/client_reconnect_command.d ./src/ipc/client_reconnect_command.o ./src/ipc/get_char_command.d ./src/ipc/get_char_command.o ./src/ipc/get_double_command.d ./src/ipc/get_double_command.o ./src/ipc/get_temperature_command.d ./src/ipc/get_temperature_command.o ./src/ipc/ipc_ctrl.d ./src/ipc/ipc_ctrl.o ./src/ipc/is_channel_connected_command.d ./src/ipc/is_channel_connected_command.o ./src/ipc/is_client_connected_command.d ./src/ipc/is_client_connected_command.o ./src/ipc/is_device_connected_command.d ./src/ipc/is_device_connected_command.o ./src/ipc/user_reconnect_command.d ./src/ipc/user_reconnect_command.o
+	-$(RM) ./src/ipc/abstract_client_reconnect_command.d ./src/ipc/abstract_client_reconnect_command.o ./src/ipc/abstract_get_char_command.d ./src/ipc/abstract_get_char_command.o ./src/ipc/abstract_get_double_command.d ./src/ipc/abstract_get_double_command.o ./src/ipc/abstract_get_humidity_command.d ./src/ipc/abstract_get_humidity_command.o ./src/ipc/abstract_get_temperature_command.d ./src/ipc/abstract_get_temperature_command.o ./src/ipc/abstract_ipc_command.d ./src/ipc/abstract_ipc_command.o ./src/ipc/abstract_ipc_ctrl.d ./src/ipc/abstract_ipc_ctrl.o ./src/ipc/abstract_ipc_socket_adapter.d ./src/ipc/abstract_ipc_socket_adapter.o ./src/ipc/abstract_is_channel_connected_command.d ./src/ipc/abstract_is_channel_connected_command.o ./src/ipc/abstract_is_client_connected_command.d ./src/ipc/abstract_is_client_connected_command.o ./src/ipc/abstract_is_device_connected_command.d ./src/ipc/abstract_is_device_connected_command.o ./src/ipc/abstract_user_reconnect_command.d ./src/ipc/abstract_user_reconnect_command.o ./src/ipc/client_reconnect_command.d ./src/ipc/client_reconnect_command.o ./src/ipc/get_char_command.d ./src/ipc/get_char_command.o ./src/ipc/get_double_command.d ./src/ipc/get_double_command.o ./src/ipc/get_humidity_command.d ./src/ipc/get_humidity_command.o ./src/ipc/get_temperature_command.d ./src/ipc/get_temperature_command.o ./src/ipc/ipc_ctrl.d ./src/ipc/ipc_ctrl.o ./src/ipc/is_channel_connected_command.d ./src/ipc/is_channel_connected_command.o ./src/ipc/is_client_connected_command.d ./src/ipc/is_client_connected_command.o ./src/ipc/is_device_connected_command.d ./src/ipc/is_device_connected_command.o ./src/ipc/user_reconnect_command.d ./src/ipc/user_reconnect_command.o
 
 .PHONY: clean-src-2f-ipc
 
---
+++ supla-server/src/ipc/abstract_get_humidity_command.cpp
@@ -0,0 +1,43 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "ipc/abstract_get_humidity_command.h"
+
+supla_abstract_get_humidity_command::supla_abstract_get_humidity_command(
+    supla_abstract_ipc_socket_adapter *socket_adapter)
+    : supla_abstract_ipc_command(socket_adapter),
+      command_name("GET-HUMIDITY-VALUE:") {}
+
+const char *supla_abstract_get_humidity_command::get_command_name(void) {
+  return command_name.c_str();
+}
+
+void supla_abstract_get_humidity_command::on_command_match(const char *params) {
+  process_parameters(
+      params, [this](int user_id, int device_id, int channel_id) -> bool {
+        double value = 0;
+
+        if (get_channel_humidity_value(user_id, device_id, channel_id,
+                                       &value)) {
+          send_result("VALUE:", value);
+          return true;
+        }
+
+        return false;
+      });
+}
