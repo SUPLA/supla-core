992fb78acfffd29fd0ce7bb23c0e2ecf616d564f
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-07-06 23:45:34 +0200

    Implementation of the supla_abstractarań_command class

---
+++ supla-server/src/ipc/abstract_action_command.cpp
@@ -0,0 +1,115 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "ipc/abstract_action_command.h"
+
+#include "actions/abstract_action_config.h"
+#include "proto.h"
+
+supla_abstract_action_command::supla_abstract_action_command(
+    supla_abstract_ipc_socket_adapter *socket_adapter, int action)
+    : supla_abstract_ipc_command(socket_adapter) {
+  this->action = action;
+}
+
+const std::string supla_abstract_action_command::get_command_name(void) {
+  switch (action) {
+    case ACTION_OPEN:
+      return "ACTION-OPEN:";
+    case ACTION_CLOSE:
+      return "ACTION-CLOSE:";
+    case ACTION_TOGGLE:
+      return "ACTION-TOGGLE:";
+    case ACTION_STOP:
+      return "ACTION-STOP:";
+    case ACTION_COPY:
+      return "ACTION-COPY:";
+    case ACTION_UP_OR_STOP:
+      return "ACTION-UP-OR-STOP:";
+    case ACTION_DOWN_OR_STOP:
+      return "ACTION-DOWN-OR-STOP:";
+    case ACTION_STEP_BY_STEP:
+      return "ACTION-SBS:";
+  }
+  return "";
+}
+
+void supla_abstract_action_command::_send_result(bool success, int channel_id) {
+  if (success) {
+    send_result("OK:", channel_id);
+  } else {
+    send_result("FAIL:", channel_id);
+  }
+}
+
+void supla_abstract_action_command::on_command_match(const char *params) {
+  if (action == ACTION_COPY) {
+    int user_id = 0;
+    int device_id = 0;
+    int channel_id = 0;
+    int source_device_id = 0;
+    int source_channel_id = 0;
+
+    sscanf(params, "%i,%i,%i,%i,%i", &user_id, &device_id, &channel_id,
+           &source_device_id, &source_channel_id);
+
+    if (user_id && device_id && channel_id && source_device_id &&
+        source_channel_id) {
+      bool result = action_copy(user_id, device_id, channel_id,
+                                source_device_id, source_channel_id);
+      _send_result(result, channel_id);
+    } else {
+      send_result("UNKNOWN:", channel_id);
+    }
+
+    return;
+  }
+
+  process_parameters(
+      params, [this](int user_id, int device_id, int channel_id) -> bool {
+        bool result = false;
+
+        switch (action) {
+          case ACTION_OPEN:
+          case ACTION_CLOSE:
+            result = action_open_close(
+                user_id, device_id, channel_id, action == ACTION_OPEN,
+                get_alexa_correlation_token(), get_google_request_id());
+            break;
+          case ACTION_TOGGLE:
+            result = action_toggle(user_id, device_id, channel_id);
+            break;
+          case ACTION_STOP:
+            result = action_stop(user_id, device_id, channel_id);
+            break;
+          case ACTION_UP_OR_STOP:
+            result = action_up_or_stop(user_id, device_id, channel_id);
+            break;
+          case ACTION_DOWN_OR_STOP:
+            result = action_down_or_stop(user_id, device_id, channel_id);
+            break;
+          case ACTION_STEP_BY_STEP:
+            result = action_step_by_step(user_id, device_id, channel_id);
+            break;
+        }
+
+        _send_result(result, channel_id);
+
+        return true;
+      });
+}
---
+++ supla-server/src/ipc/abstract_action_command.h
@@ -0,0 +1,52 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef SUPLA_ABSTRACT_ACTION_COMMAND_H_
+#define SUPLA_ABSTRACT_ACTION_COMMAND_H_
+
+#include "ipc/abstract_ipc_command.h"
+
+class supla_abstract_action_command : public supla_abstract_ipc_command {
+ private:
+  int action;
+  void _send_result(bool success, int channel_id);
+
+ protected:
+  std::string get_command_name(int action);
+  virtual void on_command_match(const char *params);
+  virtual const std::string get_command_name(void);
+  virtual bool action_open_close(int user_id, int device_id, int chennal_id,
+                                 bool open, const char *alexa_correlation_token,
+                                 const char *google_request_id) = 0;
+  virtual bool action_toggle(int user_id, int device_id, int chennal_id) = 0;
+  virtual bool action_stop(int user_id, int device_id, int chennal_id) = 0;
+  virtual bool action_up_or_stop(int user_id, int device_id,
+                                 int chennal_id) = 0;
+  virtual bool action_down_or_stop(int user_id, int device_id,
+                                   int chennal_id) = 0;
+  virtual bool action_step_by_step(int user_id, int device_id,
+                                   int chennal_id) = 0;
+  virtual bool action_copy(int user_id, int device_id, int chennal_id,
+                           int source_device_id, int source_channel_id) = 0;
+
+ public:
+  explicit supla_abstract_action_command(
+      supla_abstract_ipc_socket_adapter *socket_adapter, int action);
+};
+
+#endif /* SUPLA_ABSTRACT_ACTION_COMMAND_H_ */
