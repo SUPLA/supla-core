0c761fbdc100a126d99b1eb9183dbfd19dc57863
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-07-08 16:18:33 +0200

    IPC Refactoring. ACTION-OPEN, ACTION-CLOSE, ACTION-TOGGLE, ACTION-STOP, ACTION-COPY, ACTION-UP-OR-STOP, ACTION-DOWN-OR-STOP, ACTION-SBS - Final implementation

---
+++ supla-server/src/test/doubles/ipc/ActionCommandMock.h
@@ -0,0 +1,50 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef ACTION_COMMMAND_MOCK_H_
+#define ACTION_COMMMAND_MOCK_H_
+
+#include <gmock/gmock.h>
+
+#include "ipc/abstract_action_command.h"
+
+namespace testing {
+
+class ActionCommandMock : public supla_abstract_action_command {
+ public:
+  explicit ActionCommandMock(supla_abstract_ipc_socket_adapter *socket_adapter,
+                             int action);
+  MOCK_METHOD6(action_open_close,
+               bool(int user_id, int device_id, int channel_id, bool open,
+                    const char *alexa_correlation_token,
+                    const char *google_request_id));
+  MOCK_METHOD3(action_toggle, bool(int user_id, int device_id, int channel_id));
+  MOCK_METHOD3(action_stop, bool(int user_id, int device_id, int channel_id));
+  MOCK_METHOD3(action_up_or_stop,
+               bool(int user_id, int device_id, int channel_id));
+  MOCK_METHOD3(action_down_or_stop,
+               bool(int user_id, int device_id, int channel_id));
+  MOCK_METHOD3(action_step_by_step,
+               bool(int user_id, int device_id, int channel_id));
+  MOCK_METHOD5(action_copy, bool(int user_id, int device_id, int channel_id,
+                                 int source_device_id, int source_channel_id));
+};
+
+} /* namespace testing */
+
+#endif /* ACTION_COMMMAND_MOCK_H_ */
---
+++ supla-server/src/test/ipc/ActionCommandTest.cpp
@@ -0,0 +1,222 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "ipc/ActionCommandTest.h"
+
+#include "actions/abstract_action_config.h"
+#include "proto.h"
+
+namespace testing {
+
+void ActionCommandTest::SetUp() { IpcCommandTest::SetUp(); }
+
+void ActionCommandTest::TearDown() { IpcCommandTest::TearDown(); }
+
+supla_abstract_ipc_command *ActionCommandTest::getCommand(void) { return cmd; }
+
+TEST_F(ActionCommandTest, noData) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_FALSE(cmd->process_command(buffer, sizeof(buffer), 0));
+}
+
+TEST_F(ActionCommandTest, OpenWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, true, NULL, NULL))
+      .WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-OPEN:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, OpenWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close).WillOnce(Return(false));
+  commandProcessingTest("ACTION-OPEN:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, ActionOpenWithAlexaCorrelationToken) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, true, StrEq("TokenABCD"), NULL))
+      .WillOnce(Return(true));
+
+  commandProcessingTest(
+      "ACTION-OPEN:10,20,30,ALEXA-CORRELATION-TOKEN=VG9rZW5BQkNE\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, ActionOpenWithGoogleRequestId) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, true, NULL, StrEq("RequestID")))
+      .WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-OPEN:10,20,30,GOOGLE-REQUEST-ID=UmVxdWVzdElE\n",
+                        "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, CloseWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_CLOSE);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, false, NULL, NULL))
+      .WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-CLOSE:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, CloseWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_CLOSE);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close).WillOnce(Return(false));
+  commandProcessingTest("ACTION-CLOSE:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, ActionCloseWithAlexaCorrelationToken) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_CLOSE);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, false, StrEq("TokenABCD"), NULL))
+      .WillOnce(Return(true));
+
+  commandProcessingTest(
+      "ACTION-CLOSE:10,20,30,ALEXA-CORRELATION-TOKEN=VG9rZW5BQkNE\n",
+      "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, ActionCloseWithGoogleRequestId) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_CLOSE);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close(10, 20, 30, false, NULL, StrEq("RequestID")))
+      .WillOnce(Return(true));
+
+  commandProcessingTest(
+      "ACTION-CLOSE:10,20,30,GOOGLE-REQUEST-ID=UmVxdWVzdElE\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, ToggleWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_TOGGLE);
+  cmd = &c;
+  EXPECT_CALL(c, action_toggle(10, 20, 30)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-TOGGLE:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, ToggleWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_TOGGLE);
+  cmd = &c;
+  EXPECT_CALL(c, action_toggle).WillOnce(Return(false));
+  commandProcessingTest("ACTION-TOGGLE:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, StopWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_stop(10, 20, 30)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-STOP:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, StopWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_stop).WillOnce(Return(false));
+  commandProcessingTest("ACTION-STOP:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, UpOrStopWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_UP_OR_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_up_or_stop(10, 20, 30)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-UP-OR-STOP:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, UpOrStopWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_UP_OR_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_up_or_stop).WillOnce(Return(false));
+  commandProcessingTest("ACTION-UP-OR-STOP:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, DownOrStopWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_DOWN_OR_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_down_or_stop(10, 20, 30)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-DOWN-OR-STOP:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, DownOrStopWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_DOWN_OR_STOP);
+  cmd = &c;
+  EXPECT_CALL(c, action_down_or_stop).WillOnce(Return(false));
+  commandProcessingTest("ACTION-DOWN-OR-STOP:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, StepByStepWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_STEP_BY_STEP);
+  cmd = &c;
+  EXPECT_CALL(c, action_step_by_step(10, 20, 30)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-SBS:10,20,30\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, StepByStepWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_STEP_BY_STEP);
+  cmd = &c;
+  EXPECT_CALL(c, action_step_by_step).WillOnce(Return(false));
+  commandProcessingTest("ACTION-SBS:10,20,30\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, CopyWithSuccess) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_COPY);
+  cmd = &c;
+  EXPECT_CALL(c, action_copy(10, 20, 30, 50, 100)).WillOnce(Return(true));
+
+  commandProcessingTest("ACTION-COPY:10,20,30,50,100\n", "OK:30\n");
+}
+
+TEST_F(ActionCommandTest, CopyWithFilure) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_COPY);
+  cmd = &c;
+  EXPECT_CALL(c, action_copy).WillOnce(Return(false));
+  commandProcessingTest("ACTION-COPY:10,20,30,50,100\n", "FAIL:30\n");
+}
+
+TEST_F(ActionCommandTest, noParams) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close).Times(0);
+  commandProcessingTest("ACTION-OPEN:\n", "UNKNOWN:0\n");
+}
+
+TEST_F(ActionCommandTest, paramsWithZeros) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close).Times(0);
+  commandProcessingTest("ACTION-OPEN:0,0,0\n", "UNKNOWN:0\n");
+}
+
+TEST_F(ActionCommandTest, badParams) {
+  StrictMock<ActionCommandMock> c(socketAdapter, ACTION_OPEN);
+  cmd = &c;
+  EXPECT_CALL(c, action_open_close).Times(0);
+  commandProcessingTest("ACTION-OPEN:a,10,c\n", "UNKNOWN:0\n");
+}
+
+} /* namespace testing */
