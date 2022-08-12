a00539b9eda57855840ab437334df2c17915b4a0
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-13 00:05:49 +0200

    The armeabi ABI is no longer supported

--- supla-client/Android/build_android.sh
+++ supla-client/Android/build_android.sh
@@ -14,22 +14,19 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+set -e 
+
 [ -z "$NDK_ROOT" ] && NDK_ROOT=$(echo ~/Library/Android/ndk-r23b)
 [ -z "$SUPLA_ANDROID" ] && SUPLA_ANDROID=$(echo ~/StudioProjects/supla-android)
 [ -z "$OPENSSL_ANDROID" ] && export OPENSSL_ANDROID=$(echo ~/CProjects/openssl/OpenSSL-for-Android)
 
 $NDK_ROOT/ndk-build clean
-$NDK_ROOT/ndk-build V=1 -C ./ NDK_DEBUG=1 APP_PLATFORM=android-12 APP_ABI="armeabi armeabi-v7a x86"
+$NDK_ROOT/ndk-build V=1 -C ./ NDK_DEBUG=1 APP_PLATFORM=android-21 APP_ABI="armeabi-v7a arm64-v8a x86 x86_64"
 
-cp  libs/armeabi/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/armeabi/
 cp  libs/armeabi-v7a/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/armeabi-v7a/
-cp  libs/x86/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/x86/
-
-$NDK_ROOT/ndk-build clean
-$NDK_ROOT/ndk-build V=1 -C ./ NDK_DEBUG=1 APP_PLATFORM=android-21 APP_ABI="x86_64 arm64-v8a"
-
-cp  libs/x86_64/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/x86_64/
 cp  libs/arm64-v8a/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/arm64-v8a/
+cp  libs/x86/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/x86/
+cp  libs/x86_64/libsuplaclient.so $SUPLA_ANDROID/app/src/main/libs/x86_64/
 
 cd obj/local
 rm -rf ../../symbols*
@@ -37,7 +34,7 @@ mkdir -p ../../symbols
 cp -r * ../../symbols/
 cd ../../symbols
 find ./ -name *.a -exec rm -f {} \;
-find ./ -type d -name objs-debug -exec rm -rf {} \;
+find ./ -type d -name objs-debug -prune -exec rm -rf {} \;
 rm -rf armeabi
 zip -r ../symbols.zip *
 cd ../
