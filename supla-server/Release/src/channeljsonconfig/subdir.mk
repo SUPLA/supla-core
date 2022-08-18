3dbcd8b5a5e96c07f04c70b226b3df9eabdaf289
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-18 16:04:58 +0200

    A typo in the file name

--- supla-server/Release/src/channeljsonconfig/subdir.mk
+++ supla-server/Release/src/channeljsonconfig/subdir.mk
@@ -9,7 +9,7 @@ CPP_SRCS += \
 ../src/channeljsonconfig/channel_json_config.cpp \
 ../src/channeljsonconfig/channel_json_config_getter.cpp \
 ../src/channeljsonconfig/controlling_the_gate_config.cpp \
-../src/channeljsonconfig/electicity_meter_config.cpp \
+../src/channeljsonconfig/electricity_meter_config.cpp \
 ../src/channeljsonconfig/impulse_counter_config.cpp 
 
 CPP_DEPS += \
@@ -18,7 +18,7 @@ CPP_DEPS += \
 ./src/channeljsonconfig/channel_json_config.d \
 ./src/channeljsonconfig/channel_json_config_getter.d \
 ./src/channeljsonconfig/controlling_the_gate_config.d \
-./src/channeljsonconfig/electicity_meter_config.d \
+./src/channeljsonconfig/electricity_meter_config.d \
 ./src/channeljsonconfig/impulse_counter_config.d 
 
 OBJS += \
@@ -27,7 +27,7 @@ OBJS += \
 ./src/channeljsonconfig/channel_json_config.o \
 ./src/channeljsonconfig/channel_json_config_getter.o \
 ./src/channeljsonconfig/controlling_the_gate_config.o \
-./src/channeljsonconfig/electicity_meter_config.o \
+./src/channeljsonconfig/electricity_meter_config.o \
 ./src/channeljsonconfig/impulse_counter_config.o 
 
 
@@ -43,7 +43,7 @@ src/channeljsonconfig/%.o: ../src/channeljsonconfig/%.cpp src/channeljsonconfig/
 clean: clean-src-2f-channeljsonconfig
 
 clean-src-2f-channeljsonconfig:
-	-$(RM) ./src/channeljsonconfig/abstract_channel_json_config_getter.d ./src/channeljsonconfig/abstract_channel_json_config_getter.o ./src/channeljsonconfig/action_trigger_config.d ./src/channeljsonconfig/action_trigger_config.o ./src/channeljsonconfig/channel_json_config.d ./src/channeljsonconfig/channel_json_config.o ./src/channeljsonconfig/channel_json_config_getter.d ./src/channeljsonconfig/channel_json_config_getter.o ./src/channeljsonconfig/controlling_the_gate_config.d ./src/channeljsonconfig/controlling_the_gate_config.o ./src/channeljsonconfig/electicity_meter_config.d ./src/channeljsonconfig/electicity_meter_config.o ./src/channeljsonconfig/impulse_counter_config.d ./src/channeljsonconfig/impulse_counter_config.o
+	-$(RM) ./src/channeljsonconfig/abstract_channel_json_config_getter.d ./src/channeljsonconfig/abstract_channel_json_config_getter.o ./src/channeljsonconfig/action_trigger_config.d ./src/channeljsonconfig/action_trigger_config.o ./src/channeljsonconfig/channel_json_config.d ./src/channeljsonconfig/channel_json_config.o ./src/channeljsonconfig/channel_json_config_getter.d ./src/channeljsonconfig/channel_json_config_getter.o ./src/channeljsonconfig/controlling_the_gate_config.d ./src/channeljsonconfig/controlling_the_gate_config.o ./src/channeljsonconfig/electricity_meter_config.d ./src/channeljsonconfig/electricity_meter_config.o ./src/channeljsonconfig/impulse_counter_config.d ./src/channeljsonconfig/impulse_counter_config.o
 
 .PHONY: clean-src-2f-channeljsonconfig
 
