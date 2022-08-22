db58390eceb2ba523f82fc9c94ebdad54eef8211
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-22 16:04:29 +0200

    Handler for the SUPLA_DS_CALL_DEVICE_CHANNEL_VALUE_CHANGED_C call

--- supla-server/Test/src/device/call_handler/subdir.mk
+++ supla-server/Test/src/device/call_handler/subdir.mk
@@ -7,6 +7,7 @@ CPP_SRCS += \
 ../src/device/call_handler/abstract_device_channel_value_changed.cpp \
 ../src/device/call_handler/device_channel_value_changed.cpp \
 ../src/device/call_handler/device_channel_value_changed_b.cpp \
+../src/device/call_handler/device_channel_value_changed_c.cpp \
 ../src/device/call_handler/register_device.cpp \
 ../src/device/call_handler/register_device_b.cpp \
 ../src/device/call_handler/register_device_c.cpp \
@@ -17,6 +18,7 @@ CPP_DEPS += \
 ./src/device/call_handler/abstract_device_channel_value_changed.d \
 ./src/device/call_handler/device_channel_value_changed.d \
 ./src/device/call_handler/device_channel_value_changed_b.d \
+./src/device/call_handler/device_channel_value_changed_c.d \
 ./src/device/call_handler/register_device.d \
 ./src/device/call_handler/register_device_b.d \
 ./src/device/call_handler/register_device_c.d \
@@ -27,6 +29,7 @@ OBJS += \
 ./src/device/call_handler/abstract_device_channel_value_changed.o \
 ./src/device/call_handler/device_channel_value_changed.o \
 ./src/device/call_handler/device_channel_value_changed_b.o \
+./src/device/call_handler/device_channel_value_changed_c.o \
 ./src/device/call_handler/register_device.o \
 ./src/device/call_handler/register_device_b.o \
 ./src/device/call_handler/register_device_c.o \
@@ -46,7 +49,7 @@ src/device/call_handler/%.o: ../src/device/call_handler/%.cpp src/device/call_ha
 clean: clean-src-2f-device-2f-call_handler
 
 clean-src-2f-device-2f-call_handler:
-	-$(RM) ./src/device/call_handler/abstract_device_channel_value_changed.d ./src/device/call_handler/abstract_device_channel_value_changed.o ./src/device/call_handler/device_channel_value_changed.d ./src/device/call_handler/device_channel_value_changed.o ./src/device/call_handler/device_channel_value_changed_b.d ./src/device/call_handler/device_channel_value_changed_b.o ./src/device/call_handler/register_device.d ./src/device/call_handler/register_device.o ./src/device/call_handler/register_device_b.d ./src/device/call_handler/register_device_b.o ./src/device/call_handler/register_device_c.d ./src/device/call_handler/register_device_c.o ./src/device/call_handler/register_device_d.d ./src/device/call_handler/register_device_d.o ./src/device/call_handler/register_device_e.d ./src/device/call_handler/register_device_e.o
+	-$(RM) ./src/device/call_handler/abstract_device_channel_value_changed.d ./src/device/call_handler/abstract_device_channel_value_changed.o ./src/device/call_handler/device_channel_value_changed.d ./src/device/call_handler/device_channel_value_changed.o ./src/device/call_handler/device_channel_value_changed_b.d ./src/device/call_handler/device_channel_value_changed_b.o ./src/device/call_handler/device_channel_value_changed_c.d ./src/device/call_handler/device_channel_value_changed_c.o ./src/device/call_handler/register_device.d ./src/device/call_handler/register_device.o ./src/device/call_handler/register_device_b.d ./src/device/call_handler/register_device_b.o ./src/device/call_handler/register_device_c.d ./src/device/call_handler/register_device_c.o ./src/device/call_handler/register_device_d.d ./src/device/call_handler/register_device_d.o ./src/device/call_handler/register_device_e.d ./src/device/call_handler/register_device_e.o
 
 .PHONY: clean-src-2f-device-2f-call_handler
 
