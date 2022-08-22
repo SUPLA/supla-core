fdc017adce10bc7505fc6865edeac5b7778776b9
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-22 22:18:37 +0200

    Update of mk files

--- supla-server/Test/src/device/subdir.mk
+++ supla-server/Test/src/device/subdir.mk
@@ -4,6 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../src/device/abstract_device_dao.cpp \
 ../src/device/abstract_value_getter.cpp \
 ../src/device/channel_address.cpp \
 ../src/device/channel_electricity_measurement.cpp \
@@ -16,10 +17,12 @@ CPP_SRCS += \
 ../src/device/channel_thermostat_measurement.cpp \
 ../src/device/channel_value.cpp \
 ../src/device/device.cpp \
+../src/device/device_dao.cpp \
 ../src/device/devicechannel.cpp \
 ../src/device/value_getter.cpp 
 
 CPP_DEPS += \
+./src/device/abstract_device_dao.d \
 ./src/device/abstract_value_getter.d \
 ./src/device/channel_address.d \
 ./src/device/channel_electricity_measurement.d \
@@ -32,10 +35,12 @@ CPP_DEPS += \
 ./src/device/channel_thermostat_measurement.d \
 ./src/device/channel_value.d \
 ./src/device/device.d \
+./src/device/device_dao.d \
 ./src/device/devicechannel.d \
 ./src/device/value_getter.d 
 
 OBJS += \
+./src/device/abstract_device_dao.o \
 ./src/device/abstract_value_getter.o \
 ./src/device/channel_address.o \
 ./src/device/channel_electricity_measurement.o \
@@ -48,6 +53,7 @@ OBJS += \
 ./src/device/channel_thermostat_measurement.o \
 ./src/device/channel_value.o \
 ./src/device/device.o \
+./src/device/device_dao.o \
 ./src/device/devicechannel.o \
 ./src/device/value_getter.o 
 
@@ -64,7 +70,7 @@ src/device/%.o: ../src/device/%.cpp src/device/subdir.mk
 clean: clean-src-2f-device
 
 clean-src-2f-device:
-	-$(RM) ./src/device/abstract_value_getter.d ./src/device/abstract_value_getter.o ./src/device/channel_address.d ./src/device/channel_address.o ./src/device/channel_electricity_measurement.d ./src/device/channel_electricity_measurement.o ./src/device/channel_gate_value.d ./src/device/channel_gate_value.o ./src/device/channel_ic_measurement.d ./src/device/channel_ic_measurement.o ./src/device/channel_onoff_value.d ./src/device/channel_onoff_value.o ./src/device/channel_rgbw_value.d ./src/device/channel_rgbw_value.o ./src/device/channel_rs_value.d ./src/device/channel_rs_value.o ./src/device/channel_temphum.d ./src/device/channel_temphum.o ./src/device/channel_thermostat_measurement.d ./src/device/channel_thermostat_measurement.o ./src/device/channel_value.d ./src/device/channel_value.o ./src/device/device.d ./src/device/device.o ./src/device/devicechannel.d ./src/device/devicechannel.o ./src/device/value_getter.d ./src/device/value_getter.o
+	-$(RM) ./src/device/abstract_device_dao.d ./src/device/abstract_device_dao.o ./src/device/abstract_value_getter.d ./src/device/abstract_value_getter.o ./src/device/channel_address.d ./src/device/channel_address.o ./src/device/channel_electricity_measurement.d ./src/device/channel_electricity_measurement.o ./src/device/channel_gate_value.d ./src/device/channel_gate_value.o ./src/device/channel_ic_measurement.d ./src/device/channel_ic_measurement.o ./src/device/channel_onoff_value.d ./src/device/channel_onoff_value.o ./src/device/channel_rgbw_value.d ./src/device/channel_rgbw_value.o ./src/device/channel_rs_value.d ./src/device/channel_rs_value.o ./src/device/channel_temphum.d ./src/device/channel_temphum.o ./src/device/channel_thermostat_measurement.d ./src/device/channel_thermostat_measurement.o ./src/device/channel_value.d ./src/device/channel_value.o ./src/device/device.d ./src/device/device.o ./src/device/device_dao.d ./src/device/device_dao.o ./src/device/devicechannel.d ./src/device/devicechannel.o ./src/device/value_getter.d ./src/device/value_getter.o
 
 .PHONY: clean-src-2f-device
 
