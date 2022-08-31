f195f4e2651d12969f2100b32fa6565902872205
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-31 12:18:17 +0200

    Debugging with AddressSanitizer

---
+++ supla-scheduler/Debug-Asan/src/json/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/json/cJSON.c \
+../src/json/cJSON_Utils.c 
+
+C_DEPS += \
+./src/json/cJSON.d \
+./src/json/cJSON_Utils.d 
+
+OBJS += \
+./src/json/cJSON.o \
+./src/json/cJSON_Utils.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/json/%.o: ../src/json/%.c src/json/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -D__DEBUG=1 -I$(INCMYSQL) -O2 -g3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -D_FORTIFY_SOURCE=2 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-json
+
+clean-src-2f-json:
+	-$(RM) ./src/json/cJSON.d ./src/json/cJSON.o ./src/json/cJSON_Utils.d ./src/json/cJSON_Utils.o
+
+.PHONY: clean-src-2f-json
+
