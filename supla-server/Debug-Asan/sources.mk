97a7a7b23ec1723853deb9fd10aca80a734d2a0a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-08-31 13:09:05 +0200

    Missing makefiles

---
+++ supla-server/Debug-Asan/sources.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+C++_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+C++_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src \
+src/actions \
+src/amazon \
+src/asynctask \
+src/channeljsonconfig \
+src/client \
+src/conn \
+src/converter \
+src/crypt_blowfish \
+src/device \
+src/distributedobjects \
+src/google \
+src/http \
+src/ipc \
+src/json \
+src/mqtt \
+src/scene \
+src/srpc \
+src/user \
+src/webhook \
+
