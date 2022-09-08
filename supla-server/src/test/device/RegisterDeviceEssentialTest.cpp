ad5a551b4eef3568073436f7c6ac29ef6cb54c24
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-09-08 22:28:19 +0200

    addDeviceAndChannels utest added

--- supla-server/src/test/device/RegisterDeviceEssentialTest.cpp
+++ supla-server/src/test/device/RegisterDeviceEssentialTest.cpp
@@ -40,6 +40,8 @@ void RegisterDeviceEssentialTest::SetUp() {
   ON_CALL(dba, connect).WillByDefault(Return(true));
 
   ON_CALL(dao, get_device_reg_enabled).WillByDefault(Return(true));
+
+  ON_CALL(dao, get_device_limit_left).WillByDefault(Return(100));
 }
 
 TEST_F(RegisterDeviceEssentialTest, deviceLimitExceded) {
@@ -422,10 +424,10 @@ TEST_F(RegisterDeviceEssentialTest, wrongNumberOfChannels) {
             return true;
           });
 
-  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(33));
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(55));
 
   EXPECT_CALL(
-      dao, get_device_variables(33, NotNull(), NotNull(), NotNull(), NotNull()))
+      dao, get_device_variables(55, NotNull(), NotNull(), NotNull(), NotNull()))
       .Times(1)
       .WillOnce([](int device_id, bool *device_enabled,
                    int *original_location_id, int *location_id,
@@ -435,7 +437,7 @@ TEST_F(RegisterDeviceEssentialTest, wrongNumberOfChannels) {
         return device_id;
       });
 
-  EXPECT_CALL(dao, get_device_channel_count(33)).Times(1).WillOnce(Return(1));
+  EXPECT_CALL(dao, get_device_channel_count(55)).Times(1).WillOnce(Return(1));
 
   EXPECT_CALL(srpcAdapter, sd_async_registerdevice_result(_))
       .Times(1)
@@ -489,10 +491,10 @@ TEST_F(RegisterDeviceEssentialTest, channelTypeChanged) {
             return true;
           });
 
-  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(33));
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(55));
 
   EXPECT_CALL(
-      dao, get_device_variables(33, NotNull(), NotNull(), NotNull(), NotNull()))
+      dao, get_device_variables(55, NotNull(), NotNull(), NotNull(), NotNull()))
       .Times(1)
       .WillOnce([](int device_id, bool *device_enabled,
                    int *original_location_id, int *location_id,
@@ -502,16 +504,16 @@ TEST_F(RegisterDeviceEssentialTest, channelTypeChanged) {
         return device_id;
       });
 
-  EXPECT_CALL(dao, get_device_channel_count(33)).Times(0);
+  EXPECT_CALL(dao, get_device_channel_count(55)).Times(0);
 
-  EXPECT_CALL(dao, get_channel_id_and_type(33, 0, NotNull()))
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 0, NotNull()))
       .Times(1)
       .WillOnce([](int device_id, int channel_number, int *type) {
         *type = SUPLA_CHANNELTYPE_RELAY;
         return 1;
       });
 
-  EXPECT_CALL(dao, get_channel_id_and_type(33, 1, NotNull()))
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 1, NotNull()))
       .Times(1)
       .WillOnce([](int device_id, int channel_number, int *type) {
         *type = SUPLA_CHANNELTYPE_RELAY;
@@ -580,10 +582,10 @@ TEST_F(RegisterDeviceEssentialTest,
             return true;
           });
 
-  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(33));
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(55));
 
   EXPECT_CALL(
-      dao, get_device_variables(33, NotNull(), NotNull(), NotNull(), NotNull()))
+      dao, get_device_variables(55, NotNull(), NotNull(), NotNull(), NotNull()))
       .Times(1)
       .WillOnce([](int device_id, bool *device_enabled,
                    int *original_location_id, int *location_id,
@@ -593,16 +595,16 @@ TEST_F(RegisterDeviceEssentialTest,
         return device_id;
       });
 
-  EXPECT_CALL(dao, get_device_channel_count(33)).Times(1).WillOnce(Return(2));
+  EXPECT_CALL(dao, get_device_channel_count(55)).Times(1).WillOnce(Return(2));
 
-  EXPECT_CALL(dao, get_channel_id_and_type(33, 0, NotNull()))
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 0, NotNull()))
       .Times(1)
       .WillOnce([](int device_id, int channel_number, int *type) {
         *type = SUPLA_CHANNELTYPE_RELAY;
         return 1;
       });
 
-  EXPECT_CALL(dao, get_channel_id_and_type(33, 1, NotNull()))
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 1, NotNull()))
       .Times(1)
       .WillOnce([](int device_id, int channel_number, int *type) {
         *type = SUPLA_CHANNELTYPE_DIMMER;
@@ -610,12 +612,12 @@ TEST_F(RegisterDeviceEssentialTest,
       });
 
   EXPECT_CALL(dao, on_new_device(_)).Times(0);
-  EXPECT_CALL(dao, update_device(33, 0, NotNull(), StrEq("Torch Switch"), 4567,
+  EXPECT_CALL(dao, update_device(55, 0, NotNull(), StrEq("Torch Switch"), 4567,
                                  StrEq("22.09"), SUPLA_PROTO_VERSION, 778899))
       .Times(1)
-      .WillOnce(Return(33));
+      .WillOnce(Return(55));
 
-  EXPECT_CALL(rd, on_registraction_success(33, false)).Times(1);
+  EXPECT_CALL(rd, on_registraction_success(55, false)).Times(1);
 
   EXPECT_CALL(srpcAdapter, sd_async_registerdevice_result(_))
       .Times(1)
@@ -632,6 +634,312 @@ TEST_F(RegisterDeviceEssentialTest,
 
   EXPECT_EQ(result, 0);
   EXPECT_LT(usecFromSetUp(), rd.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterDeviceEssentialTest, addChannelsToExistingDevice) {
+  TDS_SuplaRegisterDevice_E register_device_e = {};
+  register_device_e.channel_count = 2;
+  register_device_e.channels[0].Type = SUPLA_CHANNELTYPE_RELAY;
+  register_device_e.channels[1].Number = 1;
+  register_device_e.channels[1].Type = SUPLA_CHANNELTYPE_DIMMER;
+  register_device_e.channels[1].Default = SUPLA_CHANNELFNC_DIMMER;
+
+  register_device_e.GUID[0] = 1;
+  register_device_e.AuthKey[0] = 2;
+  register_device_e.Flags = 778899;
+
+  snprintf(register_device_e.Email, SUPLA_EMAIL_MAXSIZE, "%s",
+           "cheops@giza.com");
+
+  snprintf(register_device_e.SoftVer, SUPLA_SOFTVER_MAXSIZE, "%s", "22.09");
+
+  snprintf(register_device_e.Name, SUPLA_DEVICE_NAME_MAXSIZE, "%s",
+           "Torch Switch");
+
+  EXPECT_CALL(dba, connect).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(dba, disconnect).Times(1);
+  EXPECT_CALL(dba, start_transaction).Times(1);
+  EXPECT_CALL(dba, rollback).Times(0);
+  EXPECT_CALL(dba, commit).Times(1);
+
+  EXPECT_CALL(rd, get_user_id_by_email(StrEq("cheops@giza.com")))
+      .Times(1)
+      .WillOnce(Return(25));
+
+  EXPECT_CALL(rd, get_object_id(25, _, _))
+      .Times(1)
+      .WillOnce([](int user_id, const char guid[SUPLA_GUID_SIZE], int *id) {
+        *id = 55;
+        return true;
+      });
+
+  EXPECT_CALL(rd, get_authkey_hash(55, NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce(
+          [](int id, char authkey_hash[BCRYPT_HASH_MAXSIZE], bool *is_null) {
+            *is_null = true;
+            return true;
+          });
+
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(55));
+
+  EXPECT_CALL(
+      dao, get_device_variables(55, NotNull(), NotNull(), NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, bool *device_enabled,
+                   int *original_location_id, int *location_id,
+                   bool *location_enabled) {
+        *location_id = 155;
+        *location_enabled = true;
+        return device_id;
+      });
+
+  EXPECT_CALL(dao, get_device_channel_count(55)).Times(1).WillOnce(Return(2));
+
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 0, NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, int channel_number, int *type) {
+        *type = SUPLA_CHANNELTYPE_RELAY;
+        return 1;
+      });
+
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 1, NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, int channel_number, int *type) {
+        *type = 0;
+        return 0;
+      });
+
+  EXPECT_CALL(dao, add_channel(55, 1, SUPLA_CHANNELTYPE_DIMMER,
+                               SUPLA_CHANNELFNC_DIMMER, 0, 0, 0, 0, 25))
+      .Times(1)
+      .WillOnce(Return(456));
+
+  EXPECT_CALL(dao, on_channel_added(55, 456)).Times(1);
+
+  EXPECT_CALL(dao, on_new_device(_)).Times(0);
+  EXPECT_CALL(dao, update_device(55, 0, NotNull(), StrEq("Torch Switch"), 4567,
+                                 StrEq("22.09"), SUPLA_PROTO_VERSION, 778899))
+      .Times(1)
+      .WillOnce(Return(55));
+
+  EXPECT_CALL(rd, on_registraction_success(55, true)).Times(1);
+
+  EXPECT_CALL(srpcAdapter, sd_async_registerdevice_result(_))
+      .Times(1)
+      .WillOnce([](TSD_SuplaRegisterDeviceResult *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_TRUE, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  char result = rd.register_device(nullptr, &register_device_e, &srpcAdapter,
+                                   &dba, &dao, 169, 4567, 20);
+
+  EXPECT_EQ(result, 0);
+  EXPECT_LT(usecFromSetUp(), rd.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterDeviceEssentialTest, failedToAddChannel) {
+  TDS_SuplaRegisterDevice_E register_device_e = {};
+  register_device_e.channel_count = 2;
+  register_device_e.channels[0].Type = SUPLA_CHANNELTYPE_RELAY;
+  register_device_e.channels[1].Number = 1;
+  register_device_e.channels[1].Type = SUPLA_CHANNELTYPE_DIMMER;
+  register_device_e.channels[1].Default = SUPLA_CHANNELFNC_DIMMER;
+
+  register_device_e.GUID[0] = 1;
+  register_device_e.AuthKey[0] = 2;
+  register_device_e.Flags = 778899;
+
+  snprintf(register_device_e.Email, SUPLA_EMAIL_MAXSIZE, "%s",
+           "cheops@giza.com");
+
+  snprintf(register_device_e.SoftVer, SUPLA_SOFTVER_MAXSIZE, "%s", "22.09");
+
+  snprintf(register_device_e.Name, SUPLA_DEVICE_NAME_MAXSIZE, "%s",
+           "Torch Switch");
+
+  EXPECT_CALL(dba, connect).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(dba, disconnect).Times(1);
+  EXPECT_CALL(dba, start_transaction).Times(1);
+  EXPECT_CALL(dba, rollback).Times(1);
+  EXPECT_CALL(dba, commit).Times(0);
+
+  EXPECT_CALL(rd, get_user_id_by_email(StrEq("cheops@giza.com")))
+      .Times(1)
+      .WillOnce(Return(25));
+
+  EXPECT_CALL(rd, get_object_id(25, _, _))
+      .Times(1)
+      .WillOnce([](int user_id, const char guid[SUPLA_GUID_SIZE], int *id) {
+        *id = 55;
+        return true;
+      });
+
+  EXPECT_CALL(rd, get_authkey_hash(55, NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce(
+          [](int id, char authkey_hash[BCRYPT_HASH_MAXSIZE], bool *is_null) {
+            *is_null = true;
+            return true;
+          });
+
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(55));
+
+  EXPECT_CALL(
+      dao, get_device_variables(55, NotNull(), NotNull(), NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, bool *device_enabled,
+                   int *original_location_id, int *location_id,
+                   bool *location_enabled) {
+        *location_id = 155;
+        *location_enabled = true;
+        return device_id;
+      });
+
+  EXPECT_CALL(dao, get_device_channel_count).Times(0);
+
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 0, NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, int channel_number, int *type) {
+        *type = SUPLA_CHANNELTYPE_RELAY;
+        return 1;
+      });
+
+  EXPECT_CALL(dao, get_channel_id_and_type(55, 1, NotNull()))
+      .Times(1)
+      .WillOnce([](int device_id, int channel_number, int *type) {
+        *type = 0;
+        return 0;
+      });
+
+  EXPECT_CALL(dao, add_channel(55, 1, SUPLA_CHANNELTYPE_DIMMER,
+                               SUPLA_CHANNELFNC_DIMMER, 0, 0, 0, 0, 25))
+      .Times(1)
+      .WillOnce(Return(0));
+
+  EXPECT_CALL(dao, on_channel_added).Times(0);
+
+  EXPECT_CALL(dao, on_new_device).Times(0);
+  EXPECT_CALL(dao, update_device).Times(0);
+
+  EXPECT_CALL(rd, on_registraction_success).Times(0);
+
+  EXPECT_CALL(srpcAdapter, sd_async_registerdevice_result(_))
+      .Times(1)
+      .WillOnce([](TSD_SuplaRegisterDeviceResult *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_CHANNEL_CONFLICT, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  char result = rd.register_device(nullptr, &register_device_e, &srpcAdapter,
+                                   &dba, &dao, 169, 4567, 20);
+
+  EXPECT_EQ(result, 0);
+  EXPECT_GE(usecFromSetUp(), rd.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterDeviceEssentialTest, addDeviceAndChannels) {
+  TDS_SuplaRegisterDevice_E register_device_e = {};
+  register_device_e.channel_count = 2;
+  register_device_e.channels[0].Type = SUPLA_CHANNELTYPE_RELAY;
+  register_device_e.channels[1].Number = 1;
+  register_device_e.channels[1].Type = SUPLA_CHANNELTYPE_DIMMER;
+  register_device_e.channels[1].Default = SUPLA_CHANNELFNC_DIMMER;
+
+  register_device_e.GUID[0] = 1;
+  register_device_e.AuthKey[0] = 2;
+  register_device_e.Flags = 778899;
+  register_device_e.ManufacturerID = SUPLA_MFR_ACSOFTWARE;
+  register_device_e.ProductID = 177;
+
+  snprintf(register_device_e.Email, SUPLA_EMAIL_MAXSIZE, "%s",
+           "cheops@giza.com");
+
+  snprintf(register_device_e.SoftVer, SUPLA_SOFTVER_MAXSIZE, "%s", "22.09");
+
+  snprintf(register_device_e.Name, SUPLA_DEVICE_NAME_MAXSIZE, "%s",
+           "Torch Switch");
+
+  EXPECT_CALL(dba, connect).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(dba, disconnect).Times(1);
+  EXPECT_CALL(dba, start_transaction).Times(1);
+  EXPECT_CALL(dba, rollback).Times(0);
+  EXPECT_CALL(dba, commit).Times(1);
+
+  EXPECT_CALL(rd, get_user_id_by_email(StrEq("cheops@giza.com")))
+      .Times(1)
+      .WillOnce(Return(25));
+
+  EXPECT_CALL(rd, get_object_id(25, _, _))
+      .Times(1)
+      .WillOnce([](int user_id, const char guid[SUPLA_GUID_SIZE], int *id) {
+        *id = 0;
+        return true;
+      });
+
+  EXPECT_CALL(rd, get_authkey_hash).Times(0);
+
+  EXPECT_CALL(dao, get_device_id(_, _)).Times(1).WillOnce(Return(0));
+
+  EXPECT_CALL(dao, get_device_variables).Times(1).WillOnce(Return(0));
+
+  EXPECT_CALL(dao, get_location_id(25, true)).Times(1).WillOnce(Return(88));
+
+  EXPECT_CALL(dao, add_device(88, NotNull(), NotNull(), StrEq("Torch Switch"),
+                              4567, StrEq("22.09"), SUPLA_PROTO_VERSION,
+                              SUPLA_MFR_ACSOFTWARE, 177, 778899))
+      .Times(1);
+
+  EXPECT_CALL(dao, add_device(25)).Times(1).WillOnce(Return(55));
+
+  EXPECT_CALL(dao, get_channel_id_and_type(55, _, NotNull()))
+      .Times(2)
+      .WillOnce([](int device_id, int channel_number, int *type) {
+        *type = 0;
+        return 0;
+      });
+
+  EXPECT_CALL(dao,
+              add_channel(55, 0, SUPLA_CHANNELTYPE_RELAY, 0, 0, 0, 0, 0, 25))
+      .Times(1)
+      .WillOnce(Return(1));
+
+  EXPECT_CALL(dao, add_channel(55, 1, SUPLA_CHANNELTYPE_DIMMER,
+                               SUPLA_CHANNELFNC_DIMMER, 0, 0, 0, 0, 25))
+      .Times(1)
+      .WillOnce(Return(2));
+
+  EXPECT_CALL(dao, get_device_channel_count).Times(1).WillOnce(Return(2));
+
+  EXPECT_CALL(dao, on_channel_added).Times(2);
+
+  EXPECT_CALL(dao, on_new_device).Times(1);
+  EXPECT_CALL(dao, update_device).Times(0);
+
+  EXPECT_CALL(rd, on_registraction_success(55, true)).Times(1);
+
+  EXPECT_CALL(srpcAdapter, sd_async_registerdevice_result(_))
+      .Times(1)
+      .WillOnce([](TSD_SuplaRegisterDeviceResult *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_TRUE, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  char result = rd.register_device(nullptr, &register_device_e, &srpcAdapter,
+                                   &dba, &dao, 169, 4567, 20);
+
+  EXPECT_EQ(result, 0);
+  EXPECT_GE(usecFromSetUp(), rd.get_hold_time_on_failure_usec());
 }
 
 } /* namespace testing */
