5b5df9270c93f59265020562e3bc400a742d83a5
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-09-14 14:36:00 +0200

    getObjectIdWithFail utest added

--- supla-server/src/test/client/RegisterClientWithEmailAuthTest.cpp
+++ supla-server/src/test/client/RegisterClientWithEmailAuthTest.cpp
@@ -124,4 +124,40 @@ TEST_F(RegisterClientWithEmailAuthTest, emailNotFound) {
   EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
 }
 
+TEST_F(RegisterClientWithEmailAuthTest, getObjectIdWithFail) {
+  TCS_SuplaRegisterClient_D register_client_d = {};
+
+  register_client_d.GUID[0] = 1;
+  register_client_d.AuthKey[0] = 2;
+
+  snprintf(register_client_d.Email, SUPLA_EMAIL_MAXSIZE, "%s",
+           "maria@sklodowska-curie.pl");
+
+  EXPECT_CALL(dba, connect).Times(1).WillOnce(Return(true));
+  EXPECT_CALL(dba, disconnect).Times(1);
+
+  EXPECT_CALL(rc, get_user_id_by_email(StrEq("maria@sklodowska-curie.pl")))
+      .Times(1)
+      .WillOnce(Return(25));
+
+  EXPECT_CALL(rc, get_object_id(25, _, _)).Times(1).WillOnce(Return(false));
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_c(_))
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult_C *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_BAD_CREDENTIALS, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(nullptr, &register_client_d, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
+
+
 } /* namespace testing */
