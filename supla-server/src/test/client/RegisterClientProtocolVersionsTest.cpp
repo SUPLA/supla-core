01a0831190759c44f0ceb33b6289de4a5b777ef0
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-09-14 12:51:59 +0200

    Testing versions from 1-19

--- supla-server/src/test/client/RegisterClientProtocolVersionsTest.cpp
+++ supla-server/src/test/client/RegisterClientProtocolVersionsTest.cpp
@@ -25,4 +25,88 @@ RegisterClientProtocolVersionsTest::RegisterClientProtocolVersionsTest()
 
 RegisterClientProtocolVersionsTest::~RegisterClientProtocolVersionsTest() {}
 
+TEST_F(RegisterClientProtocolVersionsTest, version1) {
+  TCS_SuplaRegisterClient_B register_client_b = {};
+
+  EXPECT_CALL(srpcAdapter, get_proto_version).WillRepeatedly(Return(1));
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result)
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_GUID_ERROR, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterClientProtocolVersionsTest, version9) {
+  TCS_SuplaRegisterClient_B register_client_b = {};
+
+  EXPECT_CALL(srpcAdapter, get_proto_version).WillRepeatedly(Return(9));
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_b)
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult_B *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_GUID_ERROR, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterClientProtocolVersionsTest, version17) {
+  TCS_SuplaRegisterClient_B register_client_b = {};
+
+  EXPECT_CALL(srpcAdapter, get_proto_version).WillRepeatedly(Return(17));
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_c)
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult_C *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_GUID_ERROR, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
+TEST_F(RegisterClientProtocolVersionsTest, version19) {
+  TCS_SuplaRegisterClient_B register_client_b = {};
+
+  EXPECT_CALL(srpcAdapter, get_proto_version).WillRepeatedly(Return(19));
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_d)
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult_D *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_GUID_ERROR, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
 } /* namespace testing */
