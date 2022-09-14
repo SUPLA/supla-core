bde4ecb7486ea112728ac7cadabc203d8d0dfd29
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-09-14 15:35:00 +0200

    authFailed utest added

--- supla-server/src/test/client/RegisterClientWithAccessIdAuthTest.cpp
+++ supla-server/src/test/client/RegisterClientWithAccessIdAuthTest.cpp
@@ -44,4 +44,61 @@ TEST_F(RegisterClientWithAccessIdAuthTest, invalidGUID) {
   EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
 }
 
+TEST_F(RegisterClientWithAccessIdAuthTest, authFailed) {
+  TCS_SuplaRegisterClient_B register_client_b = {};
+
+  register_client_b.GUID[0] = 1;
+  register_client_b.AccessID = 123;
+  snprintf(register_client_b.AccessIDpwd, SUPLA_ACCESSID_PWD_MAXSIZE, "%s",
+           "abcd");
+
+  EXPECT_CALL(dba, connect).Times(2).WillRepeatedly(Return(true));
+  EXPECT_CALL(dba, disconnect).Times(2);
+  EXPECT_CALL(dba, start_transaction).Times(0);
+
+  EXPECT_CALL(client_dao, access_id_auth(123, StrEq("abcd"), NotNull(),
+                                         NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce([](int access_id, char *access_id_pwd, int *user_id,
+                   bool *is_enabled, bool *is_active) {
+        *user_id = 15;
+        *is_enabled = true;
+        *is_active = true;
+        return false;
+      });
+
+  EXPECT_CALL(client_dao, access_id_auth(345, StrEq("abcd"), NotNull(),
+                                         NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce([](int access_id, char *access_id_pwd, int *user_id,
+                   bool *is_enabled, bool *is_active) {
+        *user_id = 0;
+        *is_enabled = true;
+        *is_active = true;
+        return true;
+      });
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_c(_))
+      .Times(2)
+      .WillOnce([](TSC_SuplaRegisterClientResult_C *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_BAD_CREDENTIALS, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 456, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+
+  register_client_b.AccessID = 345;
+
+  rc.register_client(&register_client_b, nullptr, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 55, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
 } /* namespace testing */
