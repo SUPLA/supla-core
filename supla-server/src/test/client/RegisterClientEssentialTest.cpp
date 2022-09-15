ec7185abd7ec189e575faee6ccc09f507ef81368
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-09-15 21:37:27 +0200

    clientExistsAndAccessIdIsNotSet utest added

--- supla-server/src/test/client/RegisterClientEssentialTest.cpp
+++ supla-server/src/test/client/RegisterClientEssentialTest.cpp
@@ -701,4 +701,76 @@ TEST_F(RegisterClientEssentialTest, clientExistsAndIsDisabled) {
   EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
 }
 
+TEST_F(RegisterClientEssentialTest, clientExistsAndAccessIdIsNotSet) {
+  TCS_SuplaRegisterClient_D register_client_d = {};
+
+  register_client_d.GUID[0] = 1;
+  register_client_d.AuthKey[0] = 2;
+
+  snprintf(register_client_d.Email, SUPLA_EMAIL_MAXSIZE, "%s",
+           "nolan@atari.com");
+  snprintf(register_client_d.Name, SUPLA_CLIENT_NAME_MAXSIZE, "%s",
+           "Atari 800XL");
+  snprintf(register_client_d.SoftVer, SUPLA_SOFTVER_MAXSIZE, "%s", "1.0");
+
+  EXPECT_CALL(rc, get_user_id_by_email(StrEq("nolan@atari.com")))
+      .Times(1)
+      .WillOnce(Return(25));
+
+  EXPECT_CALL(rc, get_object_id(25, _, _))
+      .Times(1)
+      .WillOnce([](int user_id, const char guid[SUPLA_GUID_SIZE], int *id) {
+        *id = 987;
+        return true;
+      });
+
+  EXPECT_CALL(rc, get_authkey_hash(987, NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce(
+          [](int id, char authkey_hash[BCRYPT_HASH_MAXSIZE], bool *is_null) {
+            *is_null = true;
+            return true;
+          });
+
+  EXPECT_CALL(dba, connect).Times(1).WillOnce(Return(true));
+
+  EXPECT_CALL(dba, start_transaction).Times(1);
+
+  EXPECT_CALL(client_dao, get_client_id(25, NotNull()))
+      .Times(1)
+      .WillOnce(Return(987));
+
+  EXPECT_CALL(client_dao, get_client_variables(987, NotNull(), NotNull(),
+                                               NotNull(), NotNull()))
+      .Times(1)
+      .WillOnce(Return(987));
+
+  EXPECT_CALL(client_dao,
+              update_client(987, 0, NotNull(), StrEq("Atari 800XL"), 4567,
+                            StrEq("1.0"), SUPLA_PROTO_VERSION))
+      .Times(1)
+      .WillOnce(Return(true));
+
+  EXPECT_CALL(dba, rollback).Times(0);
+
+  EXPECT_CALL(dba, commit).Times(1);
+
+  EXPECT_CALL(dba, disconnect).Times(1);
+
+  EXPECT_CALL(srpcAdapter, sc_async_registerclient_result_c(_))
+      .Times(1)
+      .WillOnce([](TSC_SuplaRegisterClientResult_C *result) {
+        EXPECT_EQ(SUPLA_RESULTCODE_ACCESSID_NOT_ASSIGNED, result->result_code);
+        EXPECT_EQ(20, result->activity_timeout);
+        EXPECT_EQ(SUPLA_PROTO_VERSION, result->version);
+        EXPECT_EQ(SUPLA_PROTO_VERSION_MIN, result->version_min);
+        return 0;
+      });
+
+  rc.register_client(nullptr, &register_client_d, &srpcAdapter, &dba, nullptr,
+                     &client_dao, 234, 4567, 20);
+
+  EXPECT_GE(usecFromSetUp(), rc.get_hold_time_on_failure_usec());
+}
+
 } /* namespace testing */
