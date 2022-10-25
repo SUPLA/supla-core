13bb439b653945dac10d1b9d02269748d1e3dd24
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2022-10-25 11:52:10 +0200

    AutoGateClosingTest - Last needed tests added

--- supla-server/src/test/cyclictasks/AutoGateClosingTest.cpp
+++ supla-server/src/test/cyclictasks/AutoGateClosingTest.cpp
@@ -59,4 +59,116 @@ TEST_F(AutoGateClosingTest, unknownGateState) {
   agc.run(nullptr, nullptr);
 }
 
+TEST_F(AutoGateClosingTest, stillClosed) {
+  EXPECT_CALL(agc, get_dao).Times(1).WillOnce(Return(dao));
+  EXPECT_CALL(agc, get_value_getter).Times(1).WillOnce(Return(&valueGetter));
+  EXPECT_CALL(*dao, get_all_active).Times(1).WillOnce([](void) {
+    supla_abstract_auto_gate_closing_dao::item_t i;
+    i.user_id = 1;
+    i.device_id = 2;
+    i.channel_id = 3;
+    i.seconds_since_last_attempt = ATTEMPT_RETRY_TIME_SEC;
+    i.seen_open = false;
+    vector<supla_abstract_auto_gate_closing_dao::item_t> result;
+    result.push_back(i);
+    return result;
+  });
+
+  EXPECT_CALL(valueGetter, _get_value(1, 2, 3))
+      .Times(1)
+      .WillOnce([](int user_id, int device_id, int channel_id) {
+        return new supla_channel_gate_value(gsl_closed, gsl_unknown);
+      });
+
+  EXPECT_CALL(agc, close_the_gate).Times(0);
+  EXPECT_CALL(*dao, mark_gate_open).Times(0);
+
+  EXPECT_CALL(*dao, mark_gate_closed).Times(0);
+  EXPECT_CALL(*dao, set_closing_attempt).Times(0);
+
+  agc.run(nullptr, nullptr);
+}
+
+TEST_F(AutoGateClosingTest, closeSuccessfully) {
+  EXPECT_CALL(agc, get_dao).Times(1).WillOnce(Return(dao));
+  EXPECT_CALL(agc, get_value_getter).Times(1).WillOnce(Return(&valueGetter));
+  EXPECT_CALL(*dao, get_all_active).Times(1).WillOnce([](void) {
+    supla_abstract_auto_gate_closing_dao::item_t i;
+    i.user_id = 1;
+    i.device_id = 2;
+    i.channel_id = 3;
+    i.seconds_since_last_attempt = 20;
+    i.seen_open = true;
+    vector<supla_abstract_auto_gate_closing_dao::item_t> result;
+    result.push_back(i);
+    return result;
+  });
+
+  EXPECT_CALL(valueGetter, _get_value(1, 2, 3))
+      .Times(1)
+      .WillOnce([](int user_id, int device_id, int channel_id) {
+        return new supla_channel_gate_value(gsl_closed, gsl_unknown);
+      });
+
+  EXPECT_CALL(agc, close_the_gate).Times(0);
+  EXPECT_CALL(*dao, mark_gate_open).Times(0);
+
+  EXPECT_CALL(*dao, mark_gate_closed(3)).Times(1);
+  EXPECT_CALL(*dao, set_closing_attempt).Times(0);
+
+  agc.run(nullptr, nullptr);
+}
+
+TEST_F(AutoGateClosingTest, closeAttempt) {
+  EXPECT_CALL(agc, get_dao).Times(3).WillRepeatedly(Return(dao));
+  EXPECT_CALL(agc, get_value_getter)
+      .Times(3)
+      .WillRepeatedly(Return(&valueGetter));
+
+  Sequence s1;
+
+  EXPECT_CALL(*dao, get_all_active)
+      .Times(2)
+      .InSequence(s1)
+      .WillRepeatedly([](void) {
+        supla_abstract_auto_gate_closing_dao::item_t i;
+        i.user_id = 1;
+        i.device_id = 2;
+        i.channel_id = 3;
+        i.seconds_since_last_attempt = ATTEMPT_RETRY_TIME_SEC;
+        i.seen_open = true;
+        vector<supla_abstract_auto_gate_closing_dao::item_t> result;
+        result.push_back(i);
+        return result;
+      });
+
+  EXPECT_CALL(*dao, get_all_active).Times(1).InSequence(s1).WillOnce([](void) {
+    supla_abstract_auto_gate_closing_dao::item_t i;
+    i.user_id = 1;
+    i.device_id = 2;
+    i.channel_id = 3;
+    i.seconds_since_last_attempt = 0;
+    i.seen_open = true;
+    vector<supla_abstract_auto_gate_closing_dao::item_t> result;
+    result.push_back(i);
+    return result;
+  });
+
+  EXPECT_CALL(valueGetter, _get_value(1, 2, 3))
+      .Times(3)
+      .WillRepeatedly([](int user_id, int device_id, int channel_id) {
+        return new supla_channel_gate_value(gsl_open, gsl_unknown);
+      });
+
+  EXPECT_CALL(agc, close_the_gate(1, 2, 3)).Times(2);
+  EXPECT_CALL(*dao, mark_gate_open(3)).Times(3);
+
+  EXPECT_CALL(*dao, mark_gate_closed).Times(0);
+  EXPECT_CALL(*dao, set_closing_attempt(3)).Times(2);
+
+  agc.run(nullptr, nullptr);
+  agc.run(nullptr, nullptr);
+  agc.run(nullptr, nullptr);
+}
+
 }  // namespace testing
