a95f40bf84d3f717ade49d34e248c08be969da5c
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-02-17 22:12:16 +0100

    Unnecessary constructor and destructor

--- supla-server/src/test/device/ChannelTmpHumValueTest.h
+++ supla-server/src/test/device/ChannelTmpHumValueTest.h
@@ -23,10 +23,8 @@
 
 namespace testing {
 
-class ChannelTempHumValueTest {
+class ChannelTempHumValueTest : public Test {
  public:
-  virtual ~ChannelTempHumValueTest();
-  ChannelTempHumValueTest();
 };
 
 }  // namespace testing
