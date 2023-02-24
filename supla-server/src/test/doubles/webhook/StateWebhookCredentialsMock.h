472c05f93fa7fe26beef3eabca18c25abd97f764
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-02-24 22:23:05 +0100

    StateWebhookCredentialsMock class added

---
+++ supla-server/src/test/doubles/webhook/StateWebhookCredentialsMock.h
@@ -0,0 +1,54 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef STATE_WEBHOOK_CREDENTIALS_MOCK_H_
+#define STATE_WEBHOOK_CREDENTIALS_MOCK_H_
+
+#include <gmock/gmock.h>
+
+#include "webhook/abstract_state_webhook_credentials.h"
+
+namespace testing {
+
+class StateWebhookCredentialsMock
+    : public supla_abstract_state_webhook_credentials {
+ private:
+ protected:
+ public:
+  StateWebhookCredentialsMock(void);
+  virtual ~StateWebhookCredentialsMock(void);
+
+  MOCK_METHOD0(refresh_lock, void(void));
+  MOCK_METHOD0(refresh_unlock, void(void));
+  MOCK_METHOD0(get_user_short_unique_id, std::string(void));
+  MOCK_METHOD0(get_url, std::string(void));
+  MOCK_METHOD0(get_access_token, std::string(void));
+  MOCK_METHOD0(get_refresh_token, std::string(void));
+  MOCK_METHOD0(is_access_token_exists, bool(void));
+  MOCK_METHOD0(is_refresh_token_exists, bool(void));
+  MOCK_METHOD0(expires_in, int(void));
+  MOCK_METHOD0(get_set_time, struct timeval(void));
+  MOCK_METHOD3(update, void(const char *access_token, const char *refresh_token,
+                            int expires_in));
+  MOCK_METHOD0(remove, void(void));
+  MOCK_METHOD0(get_function_ids, std::list<int>(void));
+};
+
+} /* namespace testing */
+
+#endif /* STATE_WEBHOOK_CREDENTIALS_MOCK_H_ */
