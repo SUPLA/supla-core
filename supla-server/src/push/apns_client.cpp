fb1a104d32743d8edc2c6075ef7f3d65415e36db
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-04-22 15:33:10 +0200

    APNs client implementation

--- supla-server/src/push/apns_client.cpp
+++ supla-server/src/push/apns_client.cpp
@@ -18,6 +18,10 @@
 
 #include "apns_client.h"
 
+#include "json/cJSON.h"
+#include "log.h"
+
+using std::list;
 using std::string;
 
 supla_apns_client::supla_apns_client(
@@ -30,7 +34,102 @@ supla_apns_client::~supla_apns_client(void) {}
 
 _platform_e supla_apns_client::get_platform(void) { return platform_ios; }
 
+char *supla_apns_client::get_payload(void) {
+  cJSON *payload = cJSON_CreateObject();
+  cJSON *aps = cJSON_CreateObject();
+  cJSON *alert = cJSON_CreateObject();
+
+  if (!get_push_notification()->get_title().empty()) {
+    cJSON_AddStringToObject(alert, "title",
+                            get_push_notification()->get_title().c_str());
+  }
+
+  if (!get_push_notification()->get_body().empty()) {
+    cJSON_AddStringToObject(alert, "body",
+                            get_push_notification()->get_body().c_str());
+  }
+
+  cJSON_AddItemToObject(payload, "aps", aps);
+  cJSON_AddItemToObject(aps, "alert", alert);
+
+  char *result = cJSON_PrintUnformatted(payload);
+  cJSON_Delete(payload);
+  return result;
+}
+
 bool supla_apns_client::_send(supla_pn_gateway_access_token *token,
                               supla_pn_recipient *recipient) {
-  return false;
+  get_curl_adapter()->append_header("content-type: application/json");
+
+  string _token = "authorization: bearer " + token->get_token();
+  get_curl_adapter()->append_header(_token.c_str());
+
+  string topic = "apns-topic: " + token->get_extra_field("bundle_id");
+  get_curl_adapter()->append_header(topic.c_str());
+  get_curl_adapter()->append_header("apns-push-type: alert");
+
+  string endpoint = token->get_url();
+  size_t pos = endpoint.find("{device_token}");
+  if (pos != std::string::npos) {
+    endpoint.replace(pos, std::string("{device_token}").length(),
+                     recipient->get_id());
+  } else {
+    supla_log(LOG_ERR, "Url %s does not contain the string {device_token}",
+              endpoint.c_str());
+    return false;
+  }
+
+  get_curl_adapter()->set_opt_url(endpoint.c_str());
+
+  char *payload = get_payload();
+  get_curl_adapter()->set_opt_post_fields(payload);
+
+  string request_result;
+  list<string> header;
+  get_curl_adapter()->set_opt_write_data(&request_result);
+  get_curl_adapter()->set_opt_header_data(&header);
+
+  bool result = get_curl_adapter()->perform();
+  free(payload);
+
+  if (result) {
+    result = get_curl_adapter()->get_response_code() == 200;
+    if (!result) {
+      string reason;
+
+      if (get_curl_adapter()->get_response_code() == 400) {
+        cJSON *response_json = cJSON_Parse(request_result.c_str());
+        if (response_json) {
+          cJSON *reason_json = cJSON_GetObjectItem(response_json, "reason");
+          if (reason_json && cJSON_IsString(reason_json)) {
+            reason = cJSON_GetStringValue(reason_json);
+          }
+          cJSON_Delete(response_json);
+        }
+      }
+
+      if (reason == "BadDeviceToken") {
+        recipient->set_exists(false);
+      } else {
+        supla_log(LOG_ERR,
+                  "The APNs server returned an unexpected code: %i%s%s",
+                  get_curl_adapter()->get_response_code(),
+                  reason.empty() ? "" : ", reason: ",
+                  reason.empty() ? "" : reason.c_str());
+      }
+
+    } else {
+      string prefix = "apns-id: ";
+
+      for (auto it = header.rbegin(); it != header.rend(); ++it) {
+        size_t pos = it->find(prefix);
+        if (pos != std::string::npos && pos == 0) {
+          recipient->set_message_id(it->substr(pos + prefix.length()));
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
 }
