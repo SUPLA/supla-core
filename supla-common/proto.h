7b3a92ec57fb4c38ba12ae1da2b5cae3c7e5504c
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-05-17 12:32:35 +0200

    Description expanded

--- supla-common/proto.h
+++ supla-common/proto.h
@@ -1412,10 +1412,13 @@ typedef struct {
   unsigned _supla_int16_t freq;        // * 0.01 Hz
   unsigned _supla_int16_t voltage[3];  // * 0.01 V
   unsigned _supla_int16_t
-      current[3];  // * 0.001 A (0.01A FOR EM_VAR_CURRENT_OVER_65A)
-  _supla_int_t power_active[3];    // * 0.00001 W or kW
-  _supla_int_t power_reactive[3];  // * 0.00001 var or kvar
-  _supla_int_t power_apparent[3];  // * 0.00001 VA or kVA
+      current[3];  // * 0.001A (0.01A WHEN EM_VAR_CURRENT_OVER_65A)
+  _supla_int_t
+      power_active[3];  // * 0.00001W (0.01kW WHEN EM_VAR_POWER_ACTIVE_KW)
+  _supla_int_t power_reactive[3];  // * 0.00001var (0.01kvar WHEN
+                                   // EM_VAR_POWER_REACTIVE_KVAR)
+  _supla_int_t power_apparent[3];  // * 0.00001VA (0.01kVA WHEN
+                                   // EM_VAR_POWER_APPARENT_KVA)
   _supla_int16_t power_factor[3];  // * 0.001
   _supla_int16_t phase_angle[3];   // * 0.1 degree
 } TElectricityMeter_Measurement;   // v. >= 10
