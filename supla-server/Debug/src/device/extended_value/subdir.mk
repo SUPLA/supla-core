ff5725316f91763094e4709d5d80ce418e00e085
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-05-25 13:01:52 +0200

    Update mk files

--- supla-server/Debug/src/device/extended_value/subdir.mk
+++ supla-server/Debug/src/device/extended_value/subdir.mk
@@ -10,6 +10,7 @@ CPP_SRCS += \
 ../src/device/extended_value/channel_extended_value.cpp \
 ../src/device/extended_value/channel_extended_value_envelope.cpp \
 ../src/device/extended_value/channel_ic_extended_value.cpp \
+../src/device/extended_value/channel_multi_extended_value.cpp \
 ../src/device/extended_value/channel_state_extended_value.cpp \
 ../src/device/extended_value/channel_thermostat_extended_value.cpp \
 ../src/device/extended_value/timer_state_extended_value.cpp 
@@ -21,6 +22,7 @@ CPP_DEPS += \
 ./src/device/extended_value/channel_extended_value.d \
 ./src/device/extended_value/channel_extended_value_envelope.d \
 ./src/device/extended_value/channel_ic_extended_value.d \
+./src/device/extended_value/channel_multi_extended_value.d \
 ./src/device/extended_value/channel_state_extended_value.d \
 ./src/device/extended_value/channel_thermostat_extended_value.d \
 ./src/device/extended_value/timer_state_extended_value.d 
@@ -32,6 +34,7 @@ OBJS += \
 ./src/device/extended_value/channel_extended_value.o \
 ./src/device/extended_value/channel_extended_value_envelope.o \
 ./src/device/extended_value/channel_ic_extended_value.o \
+./src/device/extended_value/channel_multi_extended_value.o \
 ./src/device/extended_value/channel_state_extended_value.o \
 ./src/device/extended_value/channel_thermostat_extended_value.o \
 ./src/device/extended_value/timer_state_extended_value.o 
@@ -49,7 +52,7 @@ src/device/extended_value/%.o: ../src/device/extended_value/%.cpp src/device/ext
 clean: clean-src-2f-device-2f-extended_value
 
 clean-src-2f-device-2f-extended_value:
-	-$(RM) ./src/device/extended_value/channel_and_timer_state_extended_value.d ./src/device/extended_value/channel_and_timer_state_extended_value.o ./src/device/extended_value/channel_billing_value.d ./src/device/extended_value/channel_billing_value.o ./src/device/extended_value/channel_em_extended_value.d ./src/device/extended_value/channel_em_extended_value.o ./src/device/extended_value/channel_extended_value.d ./src/device/extended_value/channel_extended_value.o ./src/device/extended_value/channel_extended_value_envelope.d ./src/device/extended_value/channel_extended_value_envelope.o ./src/device/extended_value/channel_ic_extended_value.d ./src/device/extended_value/channel_ic_extended_value.o ./src/device/extended_value/channel_state_extended_value.d ./src/device/extended_value/channel_state_extended_value.o ./src/device/extended_value/channel_thermostat_extended_value.d ./src/device/extended_value/channel_thermostat_extended_value.o ./src/device/extended_value/timer_state_extended_value.d ./src/device/extended_value/timer_state_extended_value.o
+	-$(RM) ./src/device/extended_value/channel_and_timer_state_extended_value.d ./src/device/extended_value/channel_and_timer_state_extended_value.o ./src/device/extended_value/channel_billing_value.d ./src/device/extended_value/channel_billing_value.o ./src/device/extended_value/channel_em_extended_value.d ./src/device/extended_value/channel_em_extended_value.o ./src/device/extended_value/channel_extended_value.d ./src/device/extended_value/channel_extended_value.o ./src/device/extended_value/channel_extended_value_envelope.d ./src/device/extended_value/channel_extended_value_envelope.o ./src/device/extended_value/channel_ic_extended_value.d ./src/device/extended_value/channel_ic_extended_value.o ./src/device/extended_value/channel_multi_extended_value.d ./src/device/extended_value/channel_multi_extended_value.o ./src/device/extended_value/channel_state_extended_value.d ./src/device/extended_value/channel_state_extended_value.o ./src/device/extended_value/channel_thermostat_extended_value.d ./src/device/extended_value/channel_thermostat_extended_value.o ./src/device/extended_value/timer_state_extended_value.d ./src/device/extended_value/timer_state_extended_value.o
 
 .PHONY: clean-src-2f-device-2f-extended_value
 
