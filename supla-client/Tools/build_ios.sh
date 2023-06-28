05dfcabe654a0ceb41f79d6848a14abf81bf6657
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-06-28 12:07:14 +0200

    SDK version changed

--- supla-client/Tools/build_ios.sh
+++ supla-client/Tools/build_ios.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
-SDK="16.2"
+SDK="16.4"
 OPENSSL=""
 BUILD_CFG="Release"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
