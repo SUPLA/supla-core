0799504a04dff8b0dcde03dc34abd813c937903d
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-08-24 16:58:21 +0200

    Comment added

--- tools/cpu-profile.sh
+++ tools/cpu-profile.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# CPUPROFILESIGNAL=12 
+# https://gperftools.github.io/gperftools/cpuprofile.html
 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so CPUPROFILE=$1.prof CPUPROFILE_FREQUENCY=100000 $1
