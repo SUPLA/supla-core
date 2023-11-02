a30ce3c3ee0c652df7691540801e2a2ca8131167
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-11-02 10:13:18 +0100

    Additional scopes added

--- supla-server/src/ipc/on_channel_config_changed_command.cpp
+++ supla-server/src/ipc/on_channel_config_changed_command.cpp
@@ -30,7 +30,18 @@ supla_on_channel_config_changed_command::
     supla_on_channel_config_changed_command(
         supla_abstract_ipc_socket_adapter *socket_adapter)
     : supla_abstract_on_channel_config_changed_command(socket_adapter) {}
+/*
+ #define CONFIG_CHANGE_SCOPE_FUNCTION 0x1
+#define CONFIG_CHANGE_SCOPE_CAPTION 0x2
+#define CONFIG_CHANGE_SCOPE_LOCATION 0x4
+#define CONFIG_CHANGE_SCOPE_VISIBILITY 0x8
+#define CONFIG_CHANGE_SCOPE_ICON 0x10
+#define CONFIG_CHANGE_SCOPE_JSON_BASIC 0x20
+#define CONFIG_CHANGE_SCOPE_JSON_WEEKLY_SCHEDULE 0x40
+#define CONFIG_CHANGE_SCOPE_JSON_ALT_WEEKLY_SCHEDULE 0x80
+#define CONFIG_CHANGE_SCOPE_RELATIONS 0x100
 
+ */
 void supla_on_channel_config_changed_command::on_channel_config_changed(
     int user_id, int device_id, int channel_id, int type, int func,
     unsigned long long scope) {
@@ -58,22 +69,22 @@ void supla_on_channel_config_changed_command::on_channel_config_changed(
     if (scope & CONFIG_CHANGE_SCOPE_FUNCTION) {
       device->reconnect();
     } else if (json_config &&
-               ((scope & CONFIG_CHANGE_SCOPE_HVAC) ||
-                (scope & CONFIG_CHANGE_SCOPE_WEEKLY_SCHEDULE) ||
-                (scope & CONFIG_CHANGE_SCOPE_ALT_WEEKLY_SCHEDULE))) {
+               ((scope & CONFIG_CHANGE_SCOPE_JSON_DEFAULT) ||
+                (scope & CONFIG_CHANGE_SCOPE_JSON_WEEKLY_SCHEDULE) ||
+                (scope & CONFIG_CHANGE_SCOPE_JSON_ALT_WEEKLY_SCHEDULE))) {
       device->get_channels()->access_channel(
           channel_id, [&](supla_device_channel *channel) -> void {
             channel->set_json_config(new supla_json_config(json_config));
 
-            if (scope & CONFIG_CHANGE_SCOPE_HVAC) {
+            if (scope & CONFIG_CHANGE_SCOPE_JSON_DEFAULT) {
               channel->send_config_to_device(SUPLA_CONFIG_TYPE_DEFAULT);
             }
 
-            if (scope & CONFIG_CHANGE_SCOPE_WEEKLY_SCHEDULE) {
+            if (scope & CONFIG_CHANGE_SCOPE_JSON_WEEKLY_SCHEDULE) {
               channel->send_config_to_device(SUPLA_CONFIG_TYPE_WEEKLY_SCHEDULE);
             }
 
-            if (scope & CONFIG_CHANGE_SCOPE_ALT_WEEKLY_SCHEDULE) {
+            if (scope & CONFIG_CHANGE_SCOPE_JSON_ALT_WEEKLY_SCHEDULE) {
               channel->send_config_to_device(
                   SUPLA_CONFIG_TYPE_ALT_WEEKLY_SCHEDULE);
             }
@@ -81,19 +92,22 @@ void supla_on_channel_config_changed_command::on_channel_config_changed(
     }
   }
 
-  if ((scope & CONFIG_CHANGE_SCOPE_OTHER) ||
-      (scope & CONFIG_CHANGE_SCOPE_FUNCTION)) {
+  if ((scope & CONFIG_CHANGE_SCOPE_FUNCTION) ||
+      (scope & CONFIG_CHANGE_SCOPE_CAPTION) ||
+      (scope & CONFIG_CHANGE_SCOPE_LOCATION) ||
+      (scope & CONFIG_CHANGE_SCOPE_VISIBILITY) ||
+      (scope & CONFIG_CHANGE_SCOPE_RELATIONS)) {
     user->reconnect(supla_caller(ctIPC), false, true);
   } else {
-    if (scope & CONFIG_CHANGE_SCOPE_HVAC) {
+    if (scope & CONFIG_CHANGE_SCOPE_JSON_DEFAULT) {
       user->get_clients()->update_json_config(
           channel_id, SUPLA_CONFIG_TYPE_DEFAULT, json_config);
     }
 
-    if ((scope & CONFIG_CHANGE_SCOPE_WEEKLY_SCHEDULE) ||
-        (scope & CONFIG_CHANGE_SCOPE_ALT_WEEKLY_SCHEDULE)) {
+    if ((scope & CONFIG_CHANGE_SCOPE_JSON_WEEKLY_SCHEDULE) ||
+        (scope & CONFIG_CHANGE_SCOPE_JSON_ALT_WEEKLY_SCHEDULE)) {
       user->get_clients()->update_json_config(
-          channel_id, CONFIG_CHANGE_SCOPE_WEEKLY_SCHEDULE, json_config);
+          channel_id, CONFIG_CHANGE_SCOPE_JSON_WEEKLY_SCHEDULE, json_config);
     }
   }
 
