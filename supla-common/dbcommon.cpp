e5307d6632a3bb2381c9df6d075aba9683997de0
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2023-12-02 22:08:26 +0100

    Changing the query to one compatible with an older version of MySQL.

--- supla-common/dbcommon.cpp
+++ supla-common/dbcommon.cpp
@@ -396,8 +396,8 @@ bool dbcommon::get_db_version(char *buffer, int buffer_size) {
 
   MYSQL_STMT *stmt = NULL;
   if (stmt_execute((void **)&stmt,
-                   "SELECT MAX(REGEXP_REPLACE(version, '[a-zA-Z\\\\\\\\]+', "
-                   "'')) FROM `migration_versions`",
+                   "SELECT MAX(SUBSTRING(version, POSITION('Version' IN "
+                   "version)+7)) FROM `migration_versions`",
                    NULL, 0, true)) {
     unsigned long size;
     MYSQL_BIND rbind[1];
