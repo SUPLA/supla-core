7cc286ef605528eff8d16758c311054df7ed071a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2024-02-27 21:23:46 +0100

    cpplint

--- supla-server/src/analyzer/general_purpose_measurement_analyzer.h
+++ supla-server/src/analyzer/general_purpose_measurement_analyzer.h
@@ -16,7 +16,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#include <chrono>
+#include <chrono>  // NOLINT
 
 #include "analyzer/abstract_data_analyzer.h"
 #include "analyzer/simple_statistics.h"
