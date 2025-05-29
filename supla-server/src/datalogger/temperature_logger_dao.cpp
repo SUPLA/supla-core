8660068fe264d9591102e2918c29e405ad9f1b7c
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2025-05-29 21:44:36 +0200

    Update temperature_logger_dao.cpp

--- supla-server/src/datalogger/temperature_logger_dao.cpp
+++ supla-server/src/datalogger/temperature_logger_dao.cpp
@@ -103,13 +103,14 @@ void supla_temperature_logger_dao::load(
       "SELECT c.id, c.type, c.func, v.value FROM `supla_dev_channel` c, "
       "`supla_dev_channel_value` v WHERE c.user_id = ? AND c.id = v.channel_id "
       "AND v.valid_to >= UTC_TIMESTAMP() AND (c.func = ? OR c.func = ? OR "
-      "c.func = ?) GROUP BY c.id";
+      "c.func = ?) AND c.type != ? GROUP BY c.id";
 
   int func1 = SUPLA_CHANNELFNC_THERMOMETER;
   int func2 = SUPLA_CHANNELFNC_HUMIDITY;
   int func3 = SUPLA_CHANNELFNC_HUMIDITYANDTEMPERATURE;
+  int type = SUPLA_CHANNELTYPE_VIRTUAL;
 
-  MYSQL_BIND pbind[4] = {};
+  MYSQL_BIND pbind[5] = {};
 
   pbind[0].buffer_type = MYSQL_TYPE_LONG;
   pbind[0].buffer = (char *)&user_id;
@@ -123,7 +124,10 @@ void supla_temperature_logger_dao::load(
   pbind[3].buffer_type = MYSQL_TYPE_LONG;
   pbind[3].buffer = (char *)&func3;
 
-  if (dba->stmt_execute((void **)&stmt, sql, pbind, 4, true)) {
+  pbind[4].buffer_type = MYSQL_TYPE_LONG;
+  pbind[4].buffer = (char *)&type;
+
+  if (dba->stmt_execute((void **)&stmt, sql, pbind, 5, true)) {
     MYSQL_BIND rbind[4] = {};
     char value[SUPLA_CHANNELVALUE_SIZE] = {};
     int channel_id = 0;
