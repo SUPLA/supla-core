07dc149331d742ae39208a546e3ce62d369d4c4b
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2025-12-17 16:40:48 +0100

    Reorganize files

--- supla-console-client/Test/src/test/integration/subdir.mk
+++ supla-console-client/Test/src/test/integration/subdir.mk
@@ -20,6 +20,7 @@ CPP_SRCS += \
 ../src/test/integration/SetLocationCaptionIntegrationTest.cpp \
 ../src/test/integration/SetRegistrationEnabledIntegrationTest.cpp \
 ../src/test/integration/SetSceneCaptionIntegrationTest.cpp \
+../src/test/integration/SqlShell.cpp \
 ../src/test/integration/SuperuserAuthorization.cpp \
 ../src/test/integration/SuperuserAuthorizationIntegrationTest.cpp 
 
@@ -40,6 +41,7 @@ CPP_DEPS += \
 ./src/test/integration/SetLocationCaptionIntegrationTest.d \
 ./src/test/integration/SetRegistrationEnabledIntegrationTest.d \
 ./src/test/integration/SetSceneCaptionIntegrationTest.d \
+./src/test/integration/SqlShell.d \
 ./src/test/integration/SuperuserAuthorization.d \
 ./src/test/integration/SuperuserAuthorizationIntegrationTest.d 
 
@@ -60,6 +62,7 @@ OBJS += \
 ./src/test/integration/SetLocationCaptionIntegrationTest.o \
 ./src/test/integration/SetRegistrationEnabledIntegrationTest.o \
 ./src/test/integration/SetSceneCaptionIntegrationTest.o \
+./src/test/integration/SqlShell.o \
 ./src/test/integration/SuperuserAuthorization.o \
 ./src/test/integration/SuperuserAuthorizationIntegrationTest.o 
 
@@ -68,7 +71,7 @@ OBJS += \
 src/test/integration/%.o: ../src/test/integration/%.cpp src/test/integration/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -D__DEBUG=1 -D__TEST=1 -DNOMYSQL=1 -D__OPENSSL_TOOLS=1 -D__SSOCKET_WRITE_TO_FILE=$(SSOCKET_WRITE_TO_FILE) -I$(SSLDIR)/include -I../src/supla-client-lib -I../src/test -O0 -g3 -Wall -fsigned-char  -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	g++ -D__DEBUG=1 -D__TEST=1 -DNOMYSQL=1 -D__OPENSSL_TOOLS=1 -D__SSOCKET_WRITE_TO_FILE=$(SSOCKET_WRITE_TO_FILE) -I$(SSLDIR)/include -I../src/supla-client-lib -I../src/test -I../src -O0 -g3 -Wall -fsigned-char  -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
@@ -76,7 +79,7 @@ src/test/integration/%.o: ../src/test/integration/%.cpp src/test/integration/sub
 clean: clean-src-2f-test-2f-integration
 
 clean-src-2f-test-2f-integration:
-	-$(RM) ./src/test/integration/ActionExecutionIntegrationTest.d ./src/test/integration/ActionExecutionIntegrationTest.o ./src/test/integration/ConnectionIntegrationTest.d ./src/test/integration/ConnectionIntegrationTest.o ./src/test/integration/GetChannelBasicCfg.d ./src/test/integration/GetChannelBasicCfg.o ./src/test/integration/GetChannelBasicCfgIntegrationTest.d ./src/test/integration/GetChannelBasicCfgIntegrationTest.o ./src/test/integration/GetVersionIntegrationTest.d ./src/test/integration/GetVersionIntegrationTest.o ./src/test/integration/IntegrationTest.d ./src/test/integration/IntegrationTest.o ./src/test/integration/MySqlShell.d ./src/test/integration/MySqlShell.o ./src/test/integration/OAuthIntegrationTest.d ./src/test/integration/OAuthIntegrationTest.o ./src/test/integration/ProperlyRegistered.d ./src/test/integration/ProperlyRegistered.o ./src/test/integration/RegistrationIntegrationTest.d ./src/test/integration/RegistrationIntegrationTest.o ./src/test/integration/SetChannelCaptionIntegrationTest.d ./src/test/integration/SetChannelCaptionIntegrationTest.o ./src/test/integration/SetChannelFunctionIntegrationTest.d ./src/test/integration/SetChannelFunctionIntegrationTest.o ./src/test/integration/SetChannelGroupCaptionIntegrationTest.d ./src/test/integration/SetChannelGroupCaptionIntegrationTest.o ./src/test/integration/SetLocationCaptionIntegrationTest.d ./src/test/integration/SetLocationCaptionIntegrationTest.o ./src/test/integration/SetRegistrationEnabledIntegrationTest.d ./src/test/integration/SetRegistrationEnabledIntegrationTest.o ./src/test/integration/SetSceneCaptionIntegrationTest.d ./src/test/integration/SetSceneCaptionIntegrationTest.o ./src/test/integration/SuperuserAuthorization.d ./src/test/integration/SuperuserAuthorization.o ./src/test/integration/SuperuserAuthorizationIntegrationTest.d ./src/test/integration/SuperuserAuthorizationIntegrationTest.o
+	-$(RM) ./src/test/integration/ActionExecutionIntegrationTest.d ./src/test/integration/ActionExecutionIntegrationTest.o ./src/test/integration/ConnectionIntegrationTest.d ./src/test/integration/ConnectionIntegrationTest.o ./src/test/integration/GetChannelBasicCfg.d ./src/test/integration/GetChannelBasicCfg.o ./src/test/integration/GetChannelBasicCfgIntegrationTest.d ./src/test/integration/GetChannelBasicCfgIntegrationTest.o ./src/test/integration/GetVersionIntegrationTest.d ./src/test/integration/GetVersionIntegrationTest.o ./src/test/integration/IntegrationTest.d ./src/test/integration/IntegrationTest.o ./src/test/integration/MySqlShell.d ./src/test/integration/MySqlShell.o ./src/test/integration/OAuthIntegrationTest.d ./src/test/integration/OAuthIntegrationTest.o ./src/test/integration/ProperlyRegistered.d ./src/test/integration/ProperlyRegistered.o ./src/test/integration/RegistrationIntegrationTest.d ./src/test/integration/RegistrationIntegrationTest.o ./src/test/integration/SetChannelCaptionIntegrationTest.d ./src/test/integration/SetChannelCaptionIntegrationTest.o ./src/test/integration/SetChannelFunctionIntegrationTest.d ./src/test/integration/SetChannelFunctionIntegrationTest.o ./src/test/integration/SetChannelGroupCaptionIntegrationTest.d ./src/test/integration/SetChannelGroupCaptionIntegrationTest.o ./src/test/integration/SetLocationCaptionIntegrationTest.d ./src/test/integration/SetLocationCaptionIntegrationTest.o ./src/test/integration/SetRegistrationEnabledIntegrationTest.d ./src/test/integration/SetRegistrationEnabledIntegrationTest.o ./src/test/integration/SetSceneCaptionIntegrationTest.d ./src/test/integration/SetSceneCaptionIntegrationTest.o ./src/test/integration/SqlShell.d ./src/test/integration/SqlShell.o ./src/test/integration/SuperuserAuthorization.d ./src/test/integration/SuperuserAuthorization.o ./src/test/integration/SuperuserAuthorizationIntegrationTest.d ./src/test/integration/SuperuserAuthorizationIntegrationTest.o
 
 .PHONY: clean-src-2f-test-2f-integration
 
