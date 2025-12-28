2079b3f0246df40c4a03725de68a61771735791a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2025-12-28 19:52:30 +0100

    Add Dockerfile for Debian 13

--- extras/docker/build.sh
+++ extras/docker/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
+docker build -t "devel/supla-core-debian13" -f Dockerfile.debian13 .
 docker build -t "devel/supla-core" .
--- extras/docker/run.sh
+++ extras/docker/run.sh
@@ -3,5 +3,11 @@
 
 mkdir -p data/mysql
 mkdir -p data/postgresql
-docker compose up -d
-docker exec -it supla-core-devel-supla-core /bin/bash
+
+docker compose up -d 
+
+if [ "$1" == "debian13" ]; then
+  docker exec -it supla-core-devel-supla-core-debian13 /bin/bash
+else
+  docker exec -it supla-core-devel-supla-core /bin/bash
+fi
