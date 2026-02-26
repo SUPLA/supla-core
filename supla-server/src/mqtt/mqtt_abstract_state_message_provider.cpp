9e99c90e96f818b7b4fcf79c2815f73aa23cd3fc
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2026-02-26 19:51:24 +0100

    Add missing binary sensors to MQTT.. Closes #526.

--- supla-server/src/mqtt/mqtt_abstract_state_message_provider.cpp
+++ supla-server/src/mqtt/mqtt_abstract_state_message_provider.cpp
@@ -1255,6 +1255,8 @@ bool supla_mqtt_abstract_state_message_provider::get_message_at_index(
     case SUPLA_CHANNELFNC_ALARMARMAMENTSENSOR:
     case SUPLA_CHANNELFNC_MOTION_SENSOR:
     case SUPLA_CHANNELFNC_BINARY_SENSOR:
+    case SUPLA_CHANNELFNC_FLOOD_SENSOR:
+    case SUPLA_CHANNELFNC_CONTAINER_LEVEL_SENSOR:
       return get_sensor_message_at_index(index, topic_prefix, topic_name,
                                          message, message_size);
 
