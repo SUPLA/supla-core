3912b1e4f65180e5ba973b3c0d9662fbeba76f17
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2026-03-20 19:15:16 +0100

    Add missing mk file

---
+++ supla-server/Test/src/test/doubles/vbt/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/test/doubles/vbt/ValueBasedTriggersMock.cpp 
+
+CPP_DEPS += \
+./src/test/doubles/vbt/ValueBasedTriggersMock.d 
+
+OBJS += \
+./src/test/doubles/vbt/ValueBasedTriggersMock.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/test/doubles/vbt/%.o: ../src/test/doubles/vbt/%.cpp src/test/doubles/vbt/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -std=c++17 -D__DEBUG=1 -DMQTTC_PAL_FILE=../src/mqtt/mqtt_pal.h -DUSE_OS_TZDB=1 -D__SUPLA_SERVER=1 -DUSE_DEPRECATED_EMEV_V1 -DUSE_DEPRECATED_EMEV_V2 -D__TEST=1 -D__OPENSSL_TOOLS=1 -D__BCRYPT=1 -I../src -I../src/external/inja/include -I../src/external/cJSON -I../src/external/MQTT-C/include -I../src/asynctask -I../src/mqtt -I$(INCMYSQL) -I../src/user -I../src/device -I../src/client -I$(SSLDIR)/include -I../src/test -O2 -g3 -Wall -fsigned-char -c -fmessage-length=0 -fstack-protector-all -D_FORTIFY_SOURCE=2 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-src-2f-test-2f-doubles-2f-vbt
+
+clean-src-2f-test-2f-doubles-2f-vbt:
+	-$(RM) ./src/test/doubles/vbt/ValueBasedTriggersMock.d ./src/test/doubles/vbt/ValueBasedTriggersMock.o
+
+.PHONY: clean-src-2f-test-2f-doubles-2f-vbt
+
